9f321fc4236b91759ff9b993a82d44caa54992eb
Erik de Castro Lopo <erikd@miles>
2004-05-27 11:22:39 +0000

    Massive overhaul of reconfigure.mk to make sure the tools are run in the right order.

--- reconfigure.mk
+++ reconfigure.mk
@@ -1,31 +1,54 @@
 #!/usr/bin/make -f
 
+# The auto tools MUST be run in the following order:
+#
+#	1.  aclocal
+#	2.  libtoolize (if you use libtool)
+#	3.  autoconf
+#	4.  autoheader (if you use autoheader)
+#	5.  automake (if you use automake)
+#
+# The following makefile runs these in the correct order according to their
+# dependancies. It also makes up for Mac OSX's fucked-upped-ness.
+
+ACLOCAL = aclocal
+
 ifneq ($(shell uname -s), Darwin)
   LIBTOOLIZE = libtoolize
 else
   # Fuck Apple! Why the hell did they rename libtoolize????
   LIBTOOLIZE = glibtoolize
+  # Fink sucks as well, but this seems necessary.
+  ACLOCAL_INC = -I /sw/share/aclocal
 endif
 
-config.status: configure
+config.status: configure src/config.h.in Makefile.in src/Makefile.in tests/Makefile.in
 	./configure --enable-gcc-werror
 
-configure: configure.ac aclocal.m4 Makefile.am src/config.h.in libtool ltmain.sh
-	automake --copy --add-missing
+configure: libtool ltmain.sh
 	autoconf
 
-src/config.h.in: configure.ac libtool
+Makefile.in: Makefile.am	
+	automake --copy --add-missing
+
+src/Makefile.in: src/Makefile.am	
+	automake --copy --add-missing
+
+tests/Makefile.in: tests/Makefile.am	
+	automake --copy --add-missing
+
+src/config.h.in: configure libtool
 	autoheader
 
 libtool ltmain.sh: aclocal.m4
-	libtoolize --copy --force
+	$(LIBTOOLIZE) --copy --force
 	
 # Need to re-run aclocal whenever acinclude.m4 is modified.
 aclocal.m4: acinclude.m4
-	aclocal
+	$(ACLOCAL) $(ACLOCAL_INC)
 
 clean:
-	rm -f libtool ltmain.sh aclocal.m4 Makefile.in src/config.h.in config.cache
+	rm -f libtool ltmain.sh aclocal.m4 Makefile.in src/config.h.in config.cache config.status
 
 
 # Do not edit or modify anything in this comment block.
@@ -33,4 +56,3 @@ clean:
 # revision control system.
 #
 # arch-tag: 2b02bfd0-d5ed-489b-a554-2bf36903cca9
-
