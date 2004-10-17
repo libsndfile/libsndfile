c7a3c73d0c2f98b374148f919c99e9f366277fa1
Erik de Castro Lopo <erikd@miles>
2004-10-17 01:30:53 +0000

    Update ChangeLog.

--- src/sd2.c
+++ src/sd2.c
@@ -16,7 +16,13 @@
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+/*
+** Documentation on the Mac resource fork was obtained here :
+** http://developer.apple.com/documentation/mac/MoreToolbox/MoreToolbox-99.html
+*/
+
 #include	<stdio.h>
+#include	<stdlib.h>
 #include	<string.h>
 
 #include	"sndfile.h"
@@ -25,7 +31,7 @@
 #include	"common.h"
 
 
-#if (ENABLE_EXPERIMENTAL_CODE == 0)
+#if (0 && ENABLE_EXPERIMENTAL_CODE == 0)
 
 int
 sd2_open	(SF_PRIVATE *psf)
@@ -40,7 +46,15 @@ sd2_open	(SF_PRIVATE *psf)
  * Macros to handle big/little endian issues.
 */
 
-#define	Sd2f_MARKER		MAKE_MARKER ('S', 'd', '2', 'f')
+#define	RSRC_START_MARKER	MAKE_MARKER (0, 0, 1, 0)
+#define	Sd2f_MARKER			MAKE_MARKER ('S', 'd', '2', 'f')
+#define	Sd2a_MARKER			MAKE_MARKER ('S', 'd', '2', 'a')
+#define	ALCH_MARKER			MAKE_MARKER ('A', 'L', 'C', 'H')
+
+
+#define	SFE_SD2_BAD_RSRC_MARKER		666
+#define SFE_SD2_BAD_SD2_MARKER		667
+#define SFE_SD2_BAD_RSRC			668
 
 /*------------------------------------------------------------------------------
  * Typedefs for file chunks.
@@ -53,17 +67,92 @@ sd2_open	(SF_PRIVATE *psf)
 */
 
 static int	sd2_close	(SF_PRIVATE *psf) ;
+static int	parse_rsrc_fork (SF_PRIVATE *psf) ;
 
 /*------------------------------------------------------------------------------
 ** Public functions.
 */
 
+static int
+parse_rsrc_fork (SF_PRIVATE *psf)
+{	unsigned char byte ;
+	int marker, marker2 ;
+	int rsrc_data_offset, rsrc_map_offset, rsrc_data_length, rsrc_map_length ;
+	int rsrc_filelen ;
+	
+	rsrc_filelen = psf_get_filelen (psf) ;
+	psf_log_printf (psf, "Resource length : %d (0x%X)\n", rsrc_filelen, rsrc_filelen) ;
+
+	/* Reset the header storage because we have changed to the rsrcdes. */
+	psf->headindex = psf->headend = 0 ;
+
+	psf_binheader_readf (psf, "Ep4444", 0, &rsrc_data_offset, &rsrc_map_offset, &rsrc_data_length, &rsrc_map_length) ;
+	psf_log_printf (psf,
+			" data offset : 0x%X\n map  offset : 0x%X\n data length : 0x%X\n map  length : 0x%X\n",
+			rsrc_data_offset, rsrc_map_offset, rsrc_data_length, rsrc_map_length) ;
+
+	if (rsrc_data_offset + rsrc_data_length != rsrc_map_offset)
+	{	psf_log_printf (psf, "rsrc_data_offset + rsrc_data_length != rsrc_map_offset\n") ;
+		return SFE_SD2_BAD_RSRC ;
+		} ;
+	
+	if (rsrc_map_offset + rsrc_map_length != rsrc_filelen)
+	{	psf_log_printf (psf, "rsrc_map_offset + rsrc_map_length != rsrc_filelen\n") ;
+		return SFE_SD2_BAD_RSRC ;
+		} ;
+	
+
+	psf_binheader_readf (psf, "Epm", 0, &marker) ;
+	
+	if (marker != RSRC_START_MARKER)
+	{	psf_log_printf (psf, "Bad Marker : %08X\n", marker) ;
+		return SFE_SD2_BAD_RSRC_MARKER ;
+		} ;
+
+	psf_log_printf (psf, "Marker : %08X\n", marker) ;
+	
+	psf_binheader_readf (psf, "Ep1", 0x30, &byte) ;
+	psf_binheader_readf (psf, "b", psf->u.cbuf, (int) byte) ;
+	psf->u.cbuf [(int) (byte + 1)] = 0 ;
+	psf_log_printf (psf, "Name (0x%X) : %s\n", byte, psf->u.cbuf) ;
+
+	psf_binheader_readf (psf, "j44", 4, &marker, &marker2) ;
+	if (marker != Sd2f_MARKER || (marker2 != Sd2a_MARKER && marker2 != ALCH_MARKER))
+	{	psf_log_printf (psf, "Bad Markers : %M %M\n", marker, marker2) ;
+		return SFE_SD2_BAD_SD2_MARKER ;
+		} ;
+
+	psf_log_printf (psf, "Markers : %M %M\n", marker, marker2) ;
+
+
+psf_log_printf (psf, "position : %X\n", (int) psf_ftell (psf)) ;
+
+	return 0 ;
+} /* parse_rsrc_fork */
+
 int
 sd2_open	(SF_PRIVATE *psf)
-{	int		marker, software, rsrc_offset, len ;
+{	int		marker, software, rsrc_offset, len, error ;
 	int 	rsrc_data_offset, rsrc_map_offset, rsrc_data_length, rsrc_map_length ;
 	char	slen ;
 	float	srate ;
+
+	if (psf->rsrcdes >= 0)
+	{	int saved_filedes ;
+	
+		saved_filedes = psf->filedes ;
+		psf->filedes = psf->rsrcdes ;
+
+		error = parse_rsrc_fork (psf) ;
+
+		psf->filedes = saved_filedes ;
+
+		/*-if (error)
+			return error ;-*/
+		} ;
+
+puts (psf->logbuffer) ;
+exit (1) ;
 
 	/* Read only so far. */
 
@@ -182,6 +271,7 @@ sd2_close	(SF_PRIVATE *psf)
 } /* sd2_close */
 
 #endif
+
 /*
 ** Do not edit or modify anything in this comment block.
 ** The arch-tag line is a file identity tag for the GNU Arch 
