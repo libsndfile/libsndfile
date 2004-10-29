82b66e761084210fef6ba357c4ec354180d123af
Erik de Castro Lopo <erikd@coltrane>
2004-10-29 23:55:27 +0000

    Win32 specific header updates.

--- Win32/config.h
+++ Win32/config.h
@@ -190,13 +190,13 @@
 #define PACKAGE_NAME "libsndfile"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libsndfile 1.0.11pre7"
+#define PACKAGE_STRING "libsndfile 1.0.11pre8"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libsndfile"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.11pre7"
+#define PACKAGE_VERSION "1.0.11pre8"
 
 /* Set to maximum allowed value of sf_count_t type. */
 #define SF_COUNT_MAX 0x7FFFFFFFFFFFFFFFi64
@@ -250,7 +250,7 @@
 #define TYPEOF_SF_COUNT_T __int64_t
 
 /* Version number of package */
-#define VERSION "1.0.11pre7"
+#define VERSION "1.0.11pre8"
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
