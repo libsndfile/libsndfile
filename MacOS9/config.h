d5f1ec319761a2d01c1766f54037789d8a195283
Erik de Castro Lopo <erikd@coltrane>
2004-11-14 21:13:09 +0000

    Docs revision number updates.

--- MacOS9/config.h
+++ MacOS9/config.h
@@ -167,13 +167,13 @@
 #define PACKAGE_NAME "libsndfile"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libsndfile 1.0.11pre7"
+#define PACKAGE_STRING "libsndfile 1.0.11pre11"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libsndfile"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.11pre7"
+#define PACKAGE_VERSION "1.0.11pre11"
 
 /* Set to maximum allowed value of sf_count_t type. */
 #define SF_COUNT_MAX 0x7FFFFFFFFFFFFFFFLL
@@ -227,7 +227,7 @@
 #define TYPEOF_SF_COUNT_T off_t
 
 /* Version number of package */
-#define VERSION "1.0.11pre7"
+#define VERSION "1.0.11pre11"
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
