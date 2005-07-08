0e45147106d689ca7a277973186c9738160dbc3e
Erik de Castro Lopo <erikd@miles>
2005-07-08 10:03:57 +0000

    common.c : Fix printing of Frames.

--- src/common.c
+++ src/common.c
@@ -1128,7 +1128,7 @@ psf_log_SF_INFO (SF_PRIVATE *psf)
 {	psf_log_printf (psf, "---------------------------------\n") ;
 
 	psf_log_printf (psf, " Sample rate :   %d\n", psf->sf.samplerate) ;
-	psf_log_printf (psf, " Frames      :   %C\n", psf->sf.frames) ;
+	psf_log_printf (psf, " Frames      :   %D\n", psf->sf.frames) ;
 	psf_log_printf (psf, " Channels    :   %d\n", psf->sf.channels) ;
 
 	psf_log_printf (psf, " Format      :   0x%X\n", psf->sf.format) ;
