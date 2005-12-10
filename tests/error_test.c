e582a8c89310dba39469e68cab64c64d9989da3b
Erik de Castro Lopo <erikd@mingus>
2005-12-10 02:43:03 +0000

    A test to make sure sf_error() returns a valid error number.

--- tests/error_test.c
+++ tests/error_test.c
@@ -19,6 +19,7 @@
 #include "sfconfig.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #if HAVE_UNISTD_H
@@ -27,30 +28,80 @@
 
 #include <sndfile.h>
 
+#include "utils.h"
+
 #define	BUFFER_SIZE		(1<<15)
 #define	SHORT_BUFFER	(256)
 
-int		main (void)
-{	const char 	*noerror, *errstr ;
-	int			k ;
+static void error_number_test (void) ;
+static void error_value_test (void) ;
 
-	noerror = sf_error_number (0) ;
-
-	printf ("Testing to see if all internal error numbers have corresponding error messages :\n") ;
-
-	for (k = 1 ; k < 1000 ; k++)
-	{	errstr = sf_error_number (k) ;
-
-		printf ("%6d : %s\n", k, errstr) ;
-
-		if (errstr == noerror)
-			break ;
-		if (strstr (errstr, "This is a bug in libsndfile."))
-			return 1 ;
-		} ;
+int
+main (void)
+{	error_number_test () ;
+	error_value_test () ;
 
 	return 0 ;
 } /* main */
+
+static void
+error_number_test (void)
+{	const char 	*noerror, *errstr ;
+	int			k ;
+
+	print_test_name ("error_number_test", "") ;
+
+	noerror = sf_error_number (0) ;
+
+	for (k = 1 ; k < 300 ; k++)
+	{	errstr = sf_error_number (k) ;
+
+		/* Test for termination condition. */
+		if (errstr == noerror)
+			break ;
+
+		/* Test for error. */
+		if (strstr (errstr, "This is a bug in libsndfile."))
+			exit (1) ;
+		} ;
+
+
+	puts ("ok") ;
+	return ;
+} /* error_number_test */
+
+static void
+error_value_test (void)
+{	static unsigned char aiff_data [0x1b0] =
+	{	'F' , 'O' , 'R' , 'M' ,
+		0x00, 0x00, 0x01, 0xA8, /* FORM length */
+
+		'A' , 'I' , 'F' , 'C' ,
+		} ;
+
+	const char *filename = "error.aiff" ;
+	SNDFILE *file ;
+	SF_INFO sfinfo ;
+	int error_num ;
+
+	print_test_name ("error_value_test", filename) ;
+
+	dump_data_to_file (filename, aiff_data, sizeof (aiff_data)) ;
+
+	file = sf_open (filename, SFM_READ, &sfinfo) ;
+	if (file != NULL)
+	{	printf ("\n\nLine %d : Should not have been able to open this file.\n\n", __LINE__) ;
+		exit (1) ;
+		} ;
+
+	if ((error_num = sf_error (NULL)) <= 1 || error_num > 300)
+	{	printf ("\n\nLine %d : Should not have had an error number of %d.\n\n", __LINE__, error_num) ;
+		exit (1) ;
+		} ;
+
+	puts ("ok") ;
+	return ;
+} /* error_value_test */
 
 /*
 ** Do not edit or modify anything in this comment block.
