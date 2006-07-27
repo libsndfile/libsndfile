576e6f6f68aff5453213c803a50aac607b14116f
Erik de Castro Lopo <erikd@mingus>
2006-07-27 10:39:08 +0000

    Add a C++ test and hook into build.

--- tests/cpp_test.cc
+++ tests/cpp_test.cc
@@ -18,7 +18,6 @@
 
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
 
 #include <sndfile.hh>
 
@@ -31,21 +30,12 @@ static double	dbuffer [100] ;
 
 static void
 create_file (const char * filename, int format)
-{	SndfileHandle file ;
+{	Sndfile file ;
 
-	if (file.refCount () != 0)
-	{	printf ("\n\n%s %d : Error : Reference count (%d) should be zero.\n\n", __func__, __LINE__, file.refCount ()) ;
-		exit (1) ;
-		} ;
-
-	file = SndfileHandle (filename, SFM_WRITE, format, 2, 48000) ;
-
-	if (file.refCount () != 1)
-	{	printf ("\n\n%s %d : Error : Reference count (%d) should be 1.\n\n", __func__, __LINE__, file.refCount ()) ;
-		exit (1) ;
-		} ;
-
-	file.setString (SF_STR_TITLE, filename) ;
+	file.sfinfo.format = format ;
+	file.sfinfo.channels = 2 ;
+	file.sfinfo.samplerate = 48000 ;
+	file.openWrite (filename) ;
 
 	/* Item write. */
 	file.write (sbuffer, ARRAY_LEN (sbuffer)) ;
@@ -54,87 +44,38 @@ create_file (const char * filename, int format)
 	file.write (dbuffer, ARRAY_LEN (dbuffer)) ;
 
 	/* Frame write. */
-	file.writef (sbuffer, ARRAY_LEN (sbuffer) / file.channels ()) ;
-	file.writef (ibuffer, ARRAY_LEN (ibuffer) / file.channels ()) ;
-	file.writef (fbuffer, ARRAY_LEN (fbuffer) / file.channels ()) ;
-	file.writef (dbuffer, ARRAY_LEN (dbuffer) / file.channels ()) ;
+	file.writef (sbuffer, ARRAY_LEN (sbuffer) / file.sfinfo.channels) ;
+	file.writef (ibuffer, ARRAY_LEN (ibuffer) / file.sfinfo.channels) ;
+	file.writef (fbuffer, ARRAY_LEN (fbuffer) / file.sfinfo.channels) ;
+	file.writef (dbuffer, ARRAY_LEN (dbuffer) / file.sfinfo.channels) ;
 
-	/* RAII takes care of the SndfileHandle. */
+	/*
+	**	An explict close() call is not really necessary as the Sndfile 
+	**	destructor closes the file anyway.
+	*/
+	file.close () ;
 } /* create_file */
 
 static void
-check_title (const SndfileHandle & file, const char * filename)
-{	const char *title = NULL ;
-
-	title = file.getString (SF_STR_TITLE) ;
-
-	if (title == NULL)
-	{	printf ("\n\n%s %d : Error : No title.\n\n", __func__, __LINE__) ;
-		exit (1) ;
-		} ;
-
-	if (strcmp (filename, title) != 0)
-	{	printf ("\n\n%s %d : Error : title '%s' should be '%s'\n\n", __func__, __LINE__, title, filename) ;
-		exit (1) ;
-		} ;
-
-	return ;
-} /* check_title */
-
-static void
 read_file (const char * filename, int format)
-{	SndfileHandle file ;
-	sf_count_t count ;
+{	Sndfile file ;
 
-	if (file)
-	{	printf ("\n\n%s %d : Error : should not be here.\n\n", __func__, __LINE__) ;
+	file.openRead (filename) ;
+
+	if (file.sfinfo.format != format)
+	{	printf ("\n\n%s %d : Error : format 0x%08x should be 0x%08x.\n\n", __func__, __LINE__, file.sfinfo.format, format) ;
 		exit (1) ;
 		} ;
 
-	file = SndfileHandle (filename) ;
-
-	if (1)
-	{	SndfileHandle file2 = file ;
-
-		if (file.refCount () != 2 || file2.refCount () != 2)
-		{	printf ("\n\n%s %d : Error : Reference count (%d) should be two.\n\n", __func__, __LINE__, file.refCount ()) ;
-			exit (1) ;
-			} ;
-		} ;
-
-	if (file.refCount () != 1)
-	{	printf ("\n\n%s %d : Error : Reference count (%d) should be one.\n\n", __func__, __LINE__, file.refCount ()) ;
+	if (file.sfinfo.channels != 2)
+	{	printf ("\n\n%s %d : Error : channels %d should be 2.\n\n", __func__, __LINE__, file.sfinfo.channels) ;
 		exit (1) ;
 		} ;
 
-	if (! file)
-	{	printf ("\n\n%s %d : Error : should not be here.\n\n", __func__, __LINE__) ;
+	if (file.sfinfo.frames != ARRAY_LEN (sbuffer) * 4)
+	{	printf ("\n\n%s %d : Error : frames %ld should be %d.\n\n", __func__, __LINE__,
+				SF_COUNT_TO_LONG (file.sfinfo.frames), ARRAY_LEN (sbuffer) * 4 / 2) ;
 		exit (1) ;
-		} ;
-
-	if (file.format () != format)
-	{	printf ("\n\n%s %d : Error : format 0x%08x should be 0x%08x.\n\n", __func__, __LINE__, file.format (), format) ;
-		exit (1) ;
-		} ;
-
-	if (file.channels () != 2)
-	{	printf ("\n\n%s %d : Error : channels %d should be 2.\n\n", __func__, __LINE__, file.channels ()) ;
-		exit (1) ;
-		} ;
-
-	if (file.frames () != ARRAY_LEN (sbuffer) * 4)
-	{	printf ("\n\n%s %d : Error : frames %ld should be %lu.\n\n", __func__, __LINE__,
-				SF_COUNT_TO_LONG (file.frames ()), (long unsigned int) ARRAY_LEN (sbuffer) * 4 / 2) ;
-		exit (1) ;
-		} ;
-
-	switch (format & SF_FORMAT_TYPEMASK)
-	{	case SF_FORMAT_AU :
-				break ;
-
-		default :
-			check_title (file, filename) ;
-			break ;
 		} ;
 
 	/* Item read. */
@@ -144,47 +85,28 @@ read_file (const char * filename, int format)
 	file.read (dbuffer, ARRAY_LEN (dbuffer)) ;
 
 	/* Frame read. */
-	file.readf (sbuffer, ARRAY_LEN (sbuffer) / file.channels ()) ;
-	file.readf (ibuffer, ARRAY_LEN (ibuffer) / file.channels ()) ;
-	file.readf (fbuffer, ARRAY_LEN (fbuffer) / file.channels ()) ;
-	file.readf (dbuffer, ARRAY_LEN (dbuffer) / file.channels ()) ;
+	file.readf (sbuffer, ARRAY_LEN (sbuffer) / file.sfinfo.channels) ;
+	file.readf (ibuffer, ARRAY_LEN (ibuffer) / file.sfinfo.channels) ;
+	file.readf (fbuffer, ARRAY_LEN (fbuffer) / file.sfinfo.channels) ;
+	file.readf (dbuffer, ARRAY_LEN (dbuffer) / file.sfinfo.channels) ;
 
-	count = file.seek (file.frames () - 10, SEEK_SET) ;
-	if (count != file.frames () - 10)
-	{	printf ("\n\n%s %d : Error : offset (%ld) should be %ld\n\n", __func__, __LINE__,
-				SF_COUNT_TO_LONG (count), SF_COUNT_TO_LONG (file.frames () - 10)) ;
-		exit (1) ;
-		} ;
-
-	count = file.read (sbuffer, ARRAY_LEN (sbuffer)) ;
-	if (count != 10 * file.channels ())
-	{	printf ("\n\n%s %d : Error : count (%ld) should be %ld\n\n", __func__, __LINE__,
-				SF_COUNT_TO_LONG (count), SF_COUNT_TO_LONG (10 * file.channels ())) ;
-		exit (1) ;
-		} ;
-
-	/* RAII takes care of the SndfileHandle. */
-} /* read_file */
-
-static void
-ceeplusplus_test (const char *filename, int format)
-{
-	print_test_name ("ceeplusplus_test", filename) ;
-
-	create_file (filename, format) ;
-	read_file (filename, format) ;
-
-	remove (filename) ;
-	puts ("ok") ;
-} /* ceeplusplus_test */
+	/*
+	**	An explict close() call is not really necessary as the Sndfile 
+	**	destructor closes the file anyway.
+	*/
+	file.close () ;
+} /* create_file */
 
 int
 main (void)
-{
-	ceeplusplus_test ("cpp_test.wav", SF_FORMAT_WAV | SF_FORMAT_PCM_16) ;
-	ceeplusplus_test ("cpp_test.aiff", SF_FORMAT_AIFF | SF_FORMAT_PCM_S8) ;
-	ceeplusplus_test ("cpp_test.au", SF_FORMAT_AU | SF_FORMAT_FLOAT) ;
+{	const char * filename = "cpp_test.wav" ;
 
+	print_test_name ("CeePlusPlus test", filename) ;
+
+	create_file (filename, SF_FORMAT_WAV | SF_FORMAT_PCM_16) ;
+	read_file (filename, SF_FORMAT_WAV | SF_FORMAT_PCM_16) ;
+
+	puts ("ok") ;
 	return 0 ;
 } /* main */
 
