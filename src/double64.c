b05c4a46aac272a58fb969a4e925bdf453f257d8
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-04-16 23:00:44 +1000

    src/double64.c : Fix clipping of double to short conversions on 64 bit systems.

--- src/double64.c
+++ src/double64.c
@@ -493,7 +493,7 @@ d2s_clip_array (const double *src, int count, short *dest, double scale)
 
 		if (CPU_CLIPS_POSITIVE == 0 && tmp > 32767.0)
 			dest [count] = SHRT_MAX ;
-		else if (CPU_CLIPS_NEGATIVE == 0 && tmp < 32768.0)
+		else if (CPU_CLIPS_NEGATIVE == 0 && tmp < -32768.0)
 			dest [count] = SHRT_MIN ;
 		else
 			dest [count] = lrint (tmp) ;
