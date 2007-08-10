b77bf80074349a674397b6d990017f07ca4a0acc
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-08-10 20:37:19 +1000

    src/sndfile.c : Ogg is no longer experiemental.

--- src/sndfile.c
+++ src/sndfile.c
@@ -2250,7 +2250,7 @@ guess_file_type (SF_PRIVATE *psf)
 	if (buffer [0] == MAKE_MARKER ('c', 'a', 'f', 'f') && buffer [2] == MAKE_MARKER ('d', 'e', 's', 'c'))
 		return SF_FORMAT_CAF ;
 
-	if (ENABLE_EXPERIMENTAL_CODE && buffer [0] == MAKE_MARKER ('O', 'g', 'g', 'S'))
+	if (buffer [0] == MAKE_MARKER ('O', 'g', 'g', 'S'))
 		return SF_FORMAT_OGG ;
 
 	if (buffer [0] == MAKE_MARKER ('A', 'L', 'a', 'w') && buffer [1] == MAKE_MARKER ('S', 'o', 'u', 'n')
