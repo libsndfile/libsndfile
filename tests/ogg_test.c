6447d603dbf20f5268feb9f2c18889c46282b9a2
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-08-10 22:06:45 +1000

    tests/ogg_test.c : Update test. Seek currently failing.

--- tests/ogg_test.c
+++ tests/ogg_test.c
@@ -30,17 +30,106 @@
 #include	"generate.h"
 
 #define	SAMPLE_RATE			44100
-#define	DATA_LENGTH			(2 * SAMPLE_RATE)
+#define	DATA_LENGTH			(SAMPLE_RATE / 8)
 
 
-int
-main (void)
+static void
+ogg_float_test (void)
 {	const char * filename = "vorbis.oga" ;
+	static float data_out [DATA_LENGTH] ;
+	static float data_in [DATA_LENGTH] ;
 
-	print_test_name ("ogg test",filename) ;
+	SNDFILE * file ;
+	SF_INFO sfinfo ;
+
+	print_test_name ("ogg_float_test", filename) ;
+
+	gen_windowed_sine_float (data_out, ARRAY_LEN (data_out), 0.95) ;
+
+	memset (&sfinfo, 0, sizeof (sfinfo)) ;
+
+	/* Set up output file type. */
+	sfinfo.format = SF_FORMAT_OGG | SF_FORMAT_VORBIS ;
+	sfinfo.channels = 1 ;
+	sfinfo.samplerate = SAMPLE_RATE ;
+
+	/* Write the output file. */
+	file = test_open_file_or_die (filename, SFM_WRITE, &sfinfo, SF_FALSE, __LINE__) ;
+	test_write_float_or_die (file, 0, data_out, ARRAY_LEN (data_out), __LINE__) ;
+	sf_close (file) ;
+
+	/* Read the file in again. */
+	memset (&sfinfo, 0, sizeof (sfinfo)) ;
+
+	file = test_open_file_or_die (filename, SFM_READ, &sfinfo, SF_FALSE, __LINE__) ;
+	test_read_float_or_die (file, 0, data_in, ARRAY_LEN (data_in), __LINE__) ;
+	sf_close (file) ;
 
 	puts ("ok") ;
 
+	/* Test seeking. */
+	print_test_name ("ogg_seek_test", filename) ;
+
+	file = test_open_file_or_die (filename, SFM_READ, &sfinfo, SF_FALSE, __LINE__) ;
+	test_seek_or_die (file, 10, SEEK_SET, 10, sfinfo.channels, __LINE__) ;
+	sf_close (file) ;
+
+	puts ("ok") ;
+
+	/*-unlink (filename) ;-*/
+} /* ogg_float_test */
+
+static void
+ogg_double_test (void)
+{	const char * filename = "vorbis.oga" ;
+	static double data_out [DATA_LENGTH] ;
+	static double data_in [DATA_LENGTH] ;
+
+	SNDFILE * file ;
+	SF_INFO sfinfo ;
+
+	print_test_name ("ogg_double_test", filename) ;
+
+	gen_windowed_sine_double (data_out, ARRAY_LEN (data_out), 0.95) ;
+
+	memset (&sfinfo, 0, sizeof (sfinfo)) ;
+
+	/* Set up output file type. */
+	sfinfo.format = SF_FORMAT_OGG | SF_FORMAT_VORBIS ;
+	sfinfo.channels = 1 ;
+	sfinfo.samplerate = SAMPLE_RATE ;
+
+	/* Write the output file. */
+	file = test_open_file_or_die (filename, SFM_WRITE, &sfinfo, SF_FALSE, __LINE__) ;
+	test_write_double_or_die (file, 0, data_out, ARRAY_LEN (data_out), __LINE__) ;
+	sf_close (file) ;
+
+	/* Read the file in again. */
+	memset (&sfinfo, 0, sizeof (sfinfo)) ;
+
+	file = test_open_file_or_die (filename, SFM_READ, &sfinfo, SF_FALSE, __LINE__) ;
+	test_read_double_or_die (file, 0, data_in, ARRAY_LEN (data_in), __LINE__) ;
+	sf_close (file) ;
+
+	puts ("ok") ;
+
+	/* Test seeking. */
+	print_test_name ("ogg_seek_test", filename) ;
+
+	file = test_open_file_or_die (filename, SFM_READ, &sfinfo, SF_FALSE, __LINE__) ;
+	test_seek_or_die (file, 10, SEEK_SET, 10, sfinfo.channels, __LINE__) ;
+	sf_close (file) ;
+
+	puts ("ok") ;
+
+	/*-unlink (filename) ;-*/
+} /* ogg_double_test */
+
+int
+main (void)
+{
+	ogg_float_test () ;
+	ogg_double_test () ;
+
 	return 0 ;
 } /* main */
-
