137e13f720064455b0aa7855211e902530c964fb
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-08-30 21:54:30 +1000

    src/VORBIS/test/test.c : Cleanup in preparation for more tests.

--- src/VORBIS/test/test.c
+++ src/VORBIS/test/test.c
@@ -22,71 +22,80 @@
 #include "util.h"
 #include "write_read.h"
 
+#define ARRAY_LEN(x)   (sizeof(x)/sizeof(x[0]))
+
 #define DATA_LEN	2048
 
 #define MAX(a,b)	((a) > (b) ? (a) : (b))
 
 
-static int check_output (const float * data_in, unsigned len);
+static int short_data_test (const char * filename, int srate) ;
 
 int
-main(void){
-  static float data_out [DATA_LEN] ;
-  static float data_in [DATA_LEN] ;
+main (void)
+{	int total_errors = 0 ;
 
-  /* Do safest and most used sample rates first. */
-  int sample_rates [] = { 44100, 48000, 32000, 22050, 16000, 96000 } ;
-  unsigned k ;
-  int errors = 0 ;
+	/* Do safest and most used sample rates first. */
+	int sample_rates [] = { 44100, 48000, 32000, 22050, 16000, 96000, 88200, 8000 } ;
+	unsigned k ;
 
-  gen_windowed_sine (data_out, ARRAY_LEN (data_out), 0.95);
+	for (k = 0 ; k < ARRAY_LEN (sample_rates) ; k ++)
+	{	char filename [64] ;
+		int errors = 0 ;
 
-  for (k = 0 ; k < ARRAY_LEN (sample_rates); k ++) {
-	char filename [64] ;
-	snprintf (filename, sizeof (filename), "vorbis_%u.oga", sample_rates [k]);
+		snprintf (filename, sizeof (filename), "vorbis_%u.oga", sample_rates [k]) ;
 
-	printf ("    %-20s : ", filename);
-	fflush (stdout);
+		printf ("    %-20s : ", filename) ;
+		fflush (stdout) ;
 
-	/* Set to know value. */
-	set_data_in (data_in, ARRAY_LEN (data_in), 3.141);
+		if (short_data_test (filename, sample_rates [k]))
+			errors ++ ;
 
-	write_vorbis_data_or_die (filename, sample_rates [k], data_out, ARRAY_LEN (data_out));
-	read_vorbis_data_or_die (filename, sample_rates [k], data_in, ARRAY_LEN (data_in));
+		if (errors == 0)
+		{	puts ("ok") ;
+			remove (filename) ;
+			} ;
 
-	if (check_output (data_in, ARRAY_LEN (data_in)) != 0)
-	  errors ++ ;
-	else {
-	  puts ("ok");
-      remove (filename);
-	}
-  }
+		total_errors += errors ;
+  		} ;
 
-  if (errors)
-    exit (1);
-
-  return 0;
-}
+	return 0 ;
+} /* main */
 
 static int
-check_output (const float * data_in, unsigned len)
-{
-  float max_abs = 0.0 ;
-  unsigned k ;
+short_data_test (const char * filename, int srate)
+{	static float data_out [DATA_LEN] ;
+	static float data_in [DATA_LEN] ;
 
-  for (k = 0 ; k < len ; k++) {
-    float temp = fabs (data_in [k]);
-    max_abs = MAX (max_abs, temp);
-  }
-	
-  if (max_abs < 0.9) {
-    printf ("Error : max_abs (%f) too small.\n", max_abs);
-    return 1 ;
-  } else if (max_abs > 1.0) {
-    printf ("Error : max_abs (%f) too big.\n", max_abs);
-    return 1 ;
-  }
+	float max_abs = 0.0 ;
+	unsigned k ;
 
-  return 0 ;
-}
+	for (k = 0 ; k < ARRAY_LEN (data_out) ; k++)
+    {    data_out [k] = sin (2.0 * k * M_PI * 1.0 / 32.0 + 0.4) ;
 
+        /* Apply Hanning Window. */
+        data_out [k] *= 0.95 * (0.5 - 0.5 * cos (2.0 * M_PI * k / (ARRAY_LEN (data_out) - 1))) ;
+        } ;
+
+	/* Set to known value. */
+	set_data_in (data_in, ARRAY_LEN (data_in), 3.141) ;
+
+	write_vorbis_data_or_die (filename, srate, data_out, ARRAY_LEN (data_out)) ;
+	read_vorbis_data_or_die (filename, srate, data_in, ARRAY_LEN (data_in)) ;
+
+	for (k = 0 ; k < ARRAY_LEN (data_in) ; k++)
+	{	float temp = fabs (data_in [k]) ;
+    	max_abs = MAX (max_abs, temp) ;
+  		} ;
+
+	if (max_abs < 0.9)
+	{	printf ("Error : max_abs (%f) too small.\n", max_abs) ;
+    	return 1 ;
+  		}
+	else if (max_abs > 1.0)
+	{	printf ("Error : max_abs (%f) too big.\n", max_abs) ;
+    	return 1 ;
+  		} ;
+
+	return 0 ;
+} /* short_data_test */
