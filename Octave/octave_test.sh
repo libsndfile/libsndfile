4999ed1edf3532aafccd2cc8222f322d53d0e492
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-09-08 09:42:42 +1000

    Octave/octave_test.sh : Disable test on OSX. Can't get it to work.

--- Octave/octave_test.sh
+++ Octave/octave_test.sh
@@ -7,17 +7,21 @@ if [ -d Octave ]; then
 	fi
 
 # Find libsndfile shared object.
-libsndfile_so_location=""
+libsndfile_lib_location=""
 
 if [ -f "../src/.libs/libsndfile.so" ]; then
-	libsndfile_so_location="../src/.libs/"
+	libsndfile_lib_location="../src/.libs/"
 elif [ -f "../src/libsndfile.so" ]; then
-	libsndfile_so_location="../src/"
+	libsndfile_lib_location="../src/"
+elif [ -f "../src/.libs/libsndfile.dylib" ]; then
+	libsndfile_lib_location="../src/.libs/"
+elif [ -f "../src/libsndfile.dylib" ]; then
+	libsndfile_lib_location="../src/"
 else
-	echo "Not able to find the libsndfile.so we've just built."
+	echo "Not able to find the libsndfile shared lib we've just built."
 	exit 1
 	fi
-libsndfile_so_location=`(cd $libsndfile_so_location && pwd)`
+libsndfile_lib_location=`(cd $libsndfile_lib_location && pwd)`
 
 
 # Find sfread.oct and sfwrite.oct
@@ -35,16 +39,20 @@ else
 case `file -b $sfread_write_oct_location/sfread.oct` in
 	ELF*)
 		;;
+	Mach*)
+		echo "Tests don't work on this platform."
+		exit 0
+		;;
 	*)
 		echo "Not able to find the sfread.oct/sfwrite.oct binaries we've just built."
 		exit 1
 		;;
 	esac
 
-# echo "libsndfile_so_location : $libsndfile_so_location"
+# echo "libsndfile_lib_location : $libsndfile_lib_location"
 # echo "sfread_write_oct_location : $sfread_write_oct_location"
 
-LD_LIBRARY_PATH="$libsndfile_so_location:$LD_LIBRARY_PATH"
+LD_LIBRARY_PATH="$libsndfile_lib_location:$LD_LIBRARY_PATH"
 
 octave_src_dir=`(cd $octave_src_dir && pwd)`
 
