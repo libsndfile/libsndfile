dab3a379435631f46cd2951cea6d6b5bcc728e8b
Erik de Castro Lopo <erikd@mega-nerd.com>
2008-10-02 09:06:45 +1000

    src/file_io.c : Remove cruft.

--- src/file_io.c
+++ src/file_io.c
@@ -592,10 +592,6 @@ psf_fsync (SF_PRIVATE *psf)
 #include <windows.h>
 #include <io.h>
 
-#ifndef HAVE_SSIZE_T
-typedef long ssize_t ;
-#endif
-
 static int psf_close_handle (HANDLE handle) ;
 static HANDLE psf_open_handle (const char * path, int mode) ;
 static sf_count_t psf_get_filelen_handle (HANDLE handle) ;
@@ -1164,10 +1160,6 @@ psf_ftruncate (SF_PRIVATE *psf, sf_count_t len)
 
 #include <io.h>
 #include <direct.h>
-
-#ifndef HAVE_SSIZE_T
-typedef long ssize_t ;
-#endif
 
 /* Win32 */ int
 psf_fopen (SF_PRIVATE *psf, const char *pathname, int open_mode)
