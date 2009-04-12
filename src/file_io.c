233d578a1f5c0a7eb605d99ea7d1f0b487fb1d47
Erik de Castro Lopo <erikd@mega-nerd.com>
2009-04-12 20:31:20 +1000

    src/file_io.c : Fix obscure seeking bug reported by Hugh Secker-Walker.

--- src/file_io.c
+++ src/file_io.c
@@ -261,10 +261,12 @@ psf_file_valid (SF_PRIVATE *psf)
 
 sf_count_t
 psf_fseek (SF_PRIVATE *psf, sf_count_t offset, int whence)
-{	sf_count_t	new_position ;
+{	sf_count_t	current_pos, new_position ;
 
 	if (psf->virtual_io)
 		return psf->vio.seek (offset, whence, psf->vio_user_data) ;
+
+	current_pos = psf_ftell (psf) ;
 
 	switch (whence)
 	{	case SEEK_SET :
@@ -289,12 +291,22 @@ psf_fseek (SF_PRIVATE *psf, sf_count_t offset, int whence)
 				offset = lseek (psf->filedes, 0, SEEK_END) + offset ;
 				break ;
 
-		default :
-				/* No need to do anything about SEEK_CUR. */
+		case SEEK_CUR :
+				/* Translate a SEEK_CUR into a SEEK_SET. */
+				offset += current_pos ;
+				whence = SEEK_SET ;
 				break ;
+
+		default :
+				/* We really should not be here. */
+				psf_log_printf (psf, "psf_fseek : whence is %d *****.\n", whence) ;
+				return 0 ;
 		} ;
 
-	new_position = lseek (psf->filedes, offset, whence) ;
+	if (current_pos != offset)
+		new_position = lseek (psf->filedes, offset, whence) ;
+	else
+		new_position = offset ;
 
 	if (new_position < 0)
 		psf_log_syserr (psf, errno) ;
