e9441b769e230b8d55c91c9073cdadf4c30dd8d5
Erik de Castro Lopo <erikd@mega-nerd.com>
2011-12-25 20:36:44 +1100

    src/common.c : Print 'unknown' if psf->sf.frames == SF_COUNT_MAX.

--- src/common.c
+++ src/common.c
@@ -1150,7 +1150,10 @@ psf_log_SF_INFO (SF_PRIVATE *psf)
 {	psf_log_printf (psf, "---------------------------------\n") ;
 
 	psf_log_printf (psf, " Sample rate :   %d\n", psf->sf.samplerate) ;
-	psf_log_printf (psf, " Frames      :   %D\n", psf->sf.frames) ;
+	if (psf->sf.frames == SF_COUNT_MAX)
+		psf_log_printf (psf, " Frames      :   unknown\n") ;
+	else
+		psf_log_printf (psf, " Frames      :   %D\n", psf->sf.frames) ;
 	psf_log_printf (psf, " Channels    :   %d\n", psf->sf.channels) ;
 
 	psf_log_printf (psf, " Format      :   0x%X\n", psf->sf.format) ;
