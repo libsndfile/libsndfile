a6c099e9b6af6f4d332da5f3c0bfe4bfbc097173
Erik de Castro Lopo <erikd@mega-nerd.com>
2012-01-20 22:41:34 +1100

    Scripts/cstyle.py : Improve style checking.

--- Scripts/cstyle.py
+++ Scripts/cstyle.py
@@ -20,57 +20,96 @@ def split_string_on_quoted_str (line):
 					return [line [:start + 1]] + split_string_on_quoted_str (line [k:])
 	return [line]
 
-class cstyle_checker:
+class comment_stripper:
+	# Strip C and C++ style comments from a series of lines.
 	def __init__ (self):
+		self.comment_nest = 0
+		self.leading_space_re = re.compile ('^(\t+| )')
+		self.trailing_space_re = re.compile ('(\t+| )$')
+
+	def comment_nesting (self):
+		return self.comment_nest
+
+	def strip (self, line):
+		orig_line = line
+		# Strip C++ style comments.
+		if self.comment_nest == 0:
+			line = re.sub ("\t*//.*", '', line)
+
+		# Strip C style comments.
+		open_comment = line.find ('/*')
+		close_comment = line.find ('*/')
+
+		if self.comment_nest > 0 and close_comment < 0:
+			# Inside a comment block that does not close on this line.
+			return ""
+
+		if open_comment >= 0 and close_comment < 0:
+			# A comment begins on this line but doesn't close on this line.
+			self.comment_nest += 1
+			return self.trailing_space_re.sub ('', line [:open_comment])
+
+		if open_comment < 0 and close_comment >= 0:
+			# Currently open comment ends on this line.
+			self.comment_nest -= 1
+			return self.trailing_space_re.sub ('', line [close_comment + 2:])
+
+		if open_comment >= 0 and close_comment > 0 and self.comment_nest == 0:
+			# Comment begins and ends on this line. Replace it with 'comment'
+			# so we don't need to check whitespace before and after the comment
+			# we're removing.
+			newline = line [:open_comment] + "comment" + line [close_comment + 2:]
+			return self.strip (newline)
+
+		return line
+
+class cstyle_checker:
+	def __init__ (self, debug):
+		self.debug = debug
 		self.error_count = 0
+		self.orig_line = ''
+		self.trailing_newline_re = re.compile ('[\r\n]+$')
 
 	def get_error_count (self):
 		return self.error_count
 
+	def check_files (self, files):
+		for filename in files:
+			self.check_file (filename)
+
 	def check_file (self, filename):
 		self.filename = filename
 		self.file = open (filename, "r")
-		self.line_num = 0
-		comment_nest = 0
 
 		self.line_num = 1
+
+		stripper = comment_stripper ()
 		while 1:
 			line = self.file.readline ()
 			if not line:
 				break
 
-			orig_line = line.rstrip (line)
-			# Delete C++ style comments.
-			line = re.sub ("([ ]{1}|\t*)//.*", '', "".join (split_string_on_quoted_str (line)))
+			line = self.trailing_newline_re.sub ('', line)
 
-			# Handle comments.
-			open_comment = line.find ('/*')
-			close_comment = line.find ('*/')
-			if comment_nest > 0 and close_comment < 0:
-				line = ""
-			elif open_comment >= 0 and close_comment < 0:
-				line = line [:open_comment]
-				comment_nest += 1
-			elif open_comment < 0 and close_comment >= 0:
-				line = line [close_comment:]
-				comment_nest -= 1
-			elif open_comment >= 0 and close_comment > 0:
-				line = line [:open_comment] + line [close_comment:]
+			self.orig_line = line
 
-			# Check for errors finding comments.
-			if comment_nest < 0 or comment_nest > 1:
-				print ("Weird")
-				sys.exit (1)
-			elif comment_nest == 0:
-				# If wer're not inside comments, check the line.
-				self.line_checks (line, orig_line)
+			line = "".join (split_string_on_quoted_str (line))
+			line = stripper.strip (line)
+
+			self.line_checks (line)
 			self.line_num += 1
 
 		self.file.close ()
 		self.filename = None
+
+		# Check for errors finding comments.
+		if stripper.comment_nesting () != 0:
+			print ("Weird")
+			sys.exit (1)
+
 		return
 
-	def line_checks (self, line, orig_line):
+	def line_checks (self, line):
 		# print (line, end = "")
 
 		# Check for error which occur in comments abd code.
@@ -106,7 +145,7 @@ class cstyle_checker:
 		if re.search ("[^\s][<>!=^/][=]{1,2}[^\s]", line):
 			self.error ("missing space around comparison")
 
-		if re.search ("[a-zA-Z0-9][<>!=^/]{1,2}[a-zA-Z0-9]", line):
+		if re.search ("[a-zA-Z0-9][<>!=^/&\|]{1,2}[a-zA-Z0-9]", line):
 			if not re.search (".*#include.*[a-zA-Z0-9]/[a-zA-Z]", line):
 				self.error ("missing space around operator")
 
@@ -124,6 +163,8 @@ class cstyle_checker:
 
 	def error (self, msg):
 		print ("%s (%d) : %s" % (self.filename, self.line_num, msg))
+		if self.debug:
+			print ("'" + self.orig_line + "'")
 		self.error_count += 1
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -132,12 +173,14 @@ if len (sys.argv) < 1:
 	print ("Usage : yada yada")
 	sys.exit (1)
 
-# Create a ner cstyle_checker object
-cstyle = cstyle_checker ()
+# Create a new cstyle_checker object
+if sys.argv [1] == '-d' or sys.argv [1] == '--debug':
+	cstyle = cstyle_checker (True)
+	cstyle.check_files (sys.argv [2:])
+else:
+	cstyle = cstyle_checker (False)
+	cstyle.check_files (sys.argv [1:])
 
-error_count = 0
-for filename in sys.argv [1:]:
-	cstyle.check_file (filename)
 
 if cstyle.get_error_count ():
 	sys.exit (1)
