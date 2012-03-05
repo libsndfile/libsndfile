3cddd9a8895e8fee2a424cd8744b12a5836e6d38
Erik de Castro Lopo <erikd@mega-nerd.com>
2012-03-05 20:18:46 +1100

    src/sfconfig.h : Make sure HAVE_X86INTRIN_H is either 1 or 0.

--- src/sfconfig.h
+++ src/sfconfig.h
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2005-2011 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Copyright (C) 2005-2012 Erik de Castro Lopo <erikd@mega-nerd.com>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
@@ -107,6 +107,10 @@
 
 #ifndef HAVE_WAITPID
 #define	HAVE_WAITPID 0
+#endif
+
+#ifndef HAVE_X86INTRIN_H
+#define HAVE_X86INTRIN_H 0
 #endif
 
 #endif
