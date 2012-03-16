e73d3ab073414ee3c2c4f4413b58e82cb3fe5bdb
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2012-03-16 16:59:49 +0100

    added myself as copyright holder
    that's for the iterator-based chunk access

--- src/chunk.c
+++ src/chunk.c
@@ -1,5 +1,6 @@
 /*
 ** Copyright (C) 2008-2012 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Copyright (C) 2012 IOhannes m zmoelnig, IEM <zmoelnig@iem.at>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
