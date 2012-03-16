b4fb6e2c40a3f4006fb51e5abba2a283279c3b37
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2012-03-16 22:59:07 +0100

    restore ID in psf->get_chunk_data

--- src/wav.c
+++ src/wav.c
@@ -1901,6 +1901,9 @@ wav_get_chunk_data (SF_PRIVATE *psf, const SF_CHUNK_ITERATOR * iterator, SF_CHUN
 	if (chunk_info->data == NULL)
 		return SFE_BAD_CHUNK_DATA_PTR ;
 
+	chunk_info->id_size = psf->rchunks.chunks [indx].id_size ;
+	memcpy (chunk_info->id, psf->rchunks.chunks [indx].id, sizeof (chunk_info->id) / sizeof (*chunk_info->id)) ;
+
 	pos = psf_ftell (psf) ;
 	psf_fseek (psf, psf->rchunks.chunks [indx].offset, SEEK_SET) ;
 	psf_fread (chunk_info->data, SF_MIN (chunk_info->datalen, psf->rchunks.chunks [indx].len), 1, psf) ;
