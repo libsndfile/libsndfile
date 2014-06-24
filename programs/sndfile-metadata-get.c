15d04a775f8ad89285f7883a25a4bbdd1e3cdd5e
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-06-24 18:27:52 +1000

    programs/sndfile-metadata-get.c : Make --all the default behaviour.
    If given a file name without any options, just behave as if it had
    been given the --all option.
    
    Suggested-by: Jan Stary <hans@stare.cz>

--- programs/sndfile-metadata-get.c
+++ programs/sndfile-metadata-get.c
@@ -61,7 +61,7 @@ main (int argc, char *argv [])
 	progname = program_name (argv [0]) ;
 
 	/* Check if we've been asked for help. */
-	if (argc <= 2 || strcmp (argv [1], "--help") == 0 || strcmp (argv [1], "-h") == 0)
+	if (argc < 2 || strcmp (argv [1], "--help") == 0 || strcmp (argv [1], "-h") == 0)
 		usage_exit (progname, 0) ;
 
 	if (argv [argc - 1][0] != '-')
@@ -100,10 +100,6 @@ main (int argc, char *argv [])
 static void
 usage_exit (const char *progname, int exit_code)
 {	printf ("\nUsage :\n  %s [options] <file>\n\nOptions:\n", progname) ;
-
-	puts (
-		"    --all                 Print all metadata.\n"
-		) ;
 
 	puts (
 		"    --bext-description    Print the 'bext' description.\n"
@@ -147,6 +143,11 @@ process_args (SNDFILE * file, const SF_BROADCAST_INFO_2K * binfo, int argc, char
 			printf ("%-20s : %s\n", name, str ? str : "") ; \
 			if (! do_all) continue ; \
 			} ;
+
+	if (argc == 0)
+	{	do_all = 1 ;
+		argc = 1 ;
+		} ;
 
 	for (k = 0 ; k < argc ; k++)
 	{	if (do_all || strcmp (argv [k], "--all") == 0)
