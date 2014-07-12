0775675d17819f68531cab17ddb5c1b5f1f5c105
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-07-12 23:44:24 +1000

    CMake : Use check_type_size as needed.

--- CMake/libsndfile.cmake
+++ CMake/libsndfile.cmake
@@ -1,4 +1,5 @@
 
+include (CheckTypeSize)
 include (TestBigEndian)
 
 function (try_compile_c_result C_FILE RESULT_NAME RESULT_PASS RESULT_FAIL)
@@ -16,5 +17,21 @@ function (try_compile_c_result C_FILE RESULT_NAME RESULT_PASS RESULT_FAIL)
 
 	endfunction ()
 
+check_type_size (double SIZEOF_DOUBLE)
+check_type_size (float SIZEOF_FLOAT)
+check_type_size (int SIZEOF_INT)
+check_type_size (int64_t SIZEOF_INT64_T)
+check_type_size (loff_t SIZEOF_LOFF_T)
+check_type_size (long SIZEOF_LONG)
+check_type_size (long\ long SIZEOF_LONG_LONG)
+check_type_size (offt64_t SIZEOF_OFF64_T)
+check_type_size (off_t SIZEOF_OFF_T)
+check_type_size (short SIZEOF_SHORT)
+check_type_size (size_t SIZEOF_SIZE_T)
+check_type_size (ssize_t SIZEOF_SSIZE_T)
+check_type_size (void* SIZEOF_VOIDP)
+check_type_size (wchar_t SIZEOF_WCHAR_T)
+
+set (SIZEOF_SF_COUNT_T ${SIZEOF_INT64_T})
 
 try_compile_c_result (CMake/compiler_is_gcc.c COMPILER_IS_GCC 1 0)
