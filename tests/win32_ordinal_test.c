5e7b135195bfbabc7429d92da76b0f0e003a3d78
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-11-23 14:02:27 +1100

    tests/win32_ordinal_test.c : Better debugging when LoadLibrary fails.

--- tests/win32_ordinal_test.c
+++ tests/win32_ordinal_test.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2006-2011 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Copyright (C) 2006-2014 Erik de Castro Lopo <erikd@mega-nerd.com>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -105,7 +105,7 @@ win32_ordinal_test (void)
 		} ;
 
 	if (hmod == NULL)
-	{	puts ("\n\nError : cannot load DLL.\n") ;
+	{	printf ("\n\nError : cannot load DLL (cwd is %s).\n", getcwd (buffer, sizeof (buffer))) ;
 		exit (1) ;
 		} ;
 
