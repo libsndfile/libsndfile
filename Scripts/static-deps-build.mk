62263c5117a17ed58d863e4bddd5b7d75ddfee64
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-11-30 16:57:52 +1100

    Scripts/static-deps-build.mk : Use flac 1.3.1.
    Also improve wgetting of tarballs.

--- Scripts/static-deps-build.mk
+++ Scripts/static-deps-build.mk
@@ -9,8 +9,8 @@ ogg_md5sum = 5c3a34309d8b98640827e5d0991a4015
 vorbis_version = libvorbis-1.3.4
 vorbis_md5sum = 55f2288055e44754275a17c9a2497391
 
-flac_version = flac-1.3.0
-flac_md5sum = 13b5c214cee8373464d3d65dee362cdd
+flac_version = flac-1.3.1
+flac_md5sum = b9922c9a0378c88d3e901b234f852698
 
 #-------------------------------------------------------------------------------
 # Code follows.
@@ -40,11 +40,23 @@ clean :
 	rm -rf Build/bin Build/include Build/lib Build/share
 	rm -f Build/Stamp/install Build/Stamp/extract Build/Stamp/md5sum
 
-Build/Stamp/tarballs :
+Build/Stamp/init :
 	mkdir -p $(stamp_dir) $(tarball_dir)
-	(cd $(tarball_dir) && wget $(download_url)ogg/$(ogg_tarball))
-	(cd $(tarball_dir) && wget $(download_url)vorbis/$(vorbis_tarball))
-	(cd $(tarball_dir) && wget $(download_url)flac/$(flac_tarball))
+	touch $@
+
+Build/Tarballs/$(flac_tarball) : Build/Stamp/init
+	(cd $(tarball_dir) && wget $(download_url)flac/$(flac_tarball) -O $(flac_tarball))
+	touch $@
+
+Build/Tarballs/$(ogg_tarball) : Build/Stamp/init
+	(cd $(tarball_dir) && wget $(download_url)ogg/$(ogg_tarball) -O $(ogg_tarball))
+	touch $@
+
+Build/Tarballs/$(vorbis_tarball) : Build/Stamp/init
+	(cd $(tarball_dir) && wget $(download_url)vorbis/$(vorbis_tarball) -O $(vorbis_tarball))
+	touch $@
+
+Build/Stamp/tarballs : Build/Tarballs/$(flac_tarball) Build/Tarballs/$(ogg_tarball) Build/Tarballs/$(vorbis_tarball)
 	touch $@
 
 Build/Stamp/md5sum : Build/Stamp/tarballs
@@ -69,6 +81,5 @@ Build/Stamp/build : Build/Stamp/install
 	PKG_CONFIG_LIBDIR=Build/lib/pkgconfig ./configure
 	sed -i 's#^EXTERNAL_CFLAGS.*#EXTERNAL_CFLAGS = -I/home/erikd/Git/libsndfile/Build/include#' src/Makefile
 	sed -i 's#^EXTERNAL_LIBS.*#EXTERNAL_LIBS = -static /home/erikd/Git/libsndfile/Build/lib/libFLAC.la /home/erikd/Git/libsndfile/Build/lib/libogg.la /home/erikd/Git/libsndfile/Build/lib/libvorbis.la /home/erikd/Git/libsndfile/Build/lib/libvorbisenc.la -dynamic #' src/Makefile
-	make
-	make check
+	make clean all check
 
