libsndfile/libsndfile

commit a1f41d90bfd2095db822da184795eadf7c0c2fe6

src/paf.c: Fix a long standing endian issue
There has been a long standing issue where something was not quite
right with the endian-ness handle of PAF files. This was difficlt to
track down because of the difficulty in getting access to a Paris Audio
system. Fortunately, Jeremy Friesner <jfriesne@gmail.com> was able to
provide some test files and a preliminary patch.

src/paf.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 1999-2014 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Copyright (C) 1999-2016 Erik de Castro Lopo <erikd@mega-nerd.com>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
@@ -496,10 +496,9 @@ paf24_read_block (SF_PRIVATE *psf, PAF24_PRIVATE *ppaf24)
 	if ((k = psf_fread (ppaf24->block, 1, ppaf24->blocksize, psf)) != ppaf24->blocksize)
 		psf_log_printf (psf, "*** Warning : short read (%d != %d).\n", k, ppaf24->blocksize) ;
 
-
 	/* Do endian swapping if necessary. */
-	if (psf->endian == SF_ENDIAN_BIG)
-		endswap_int_array 	(ppaf24->data, 8 * ppaf24->channels) ;
+	if ((CPU_IS_BIG_ENDIAN && psf->endian == SF_ENDIAN_LITTLE) || (CPU_IS_LITTLE_ENDIAN && psf->endian == SF_ENDIAN_BIG))
+		endswap_int_array ((int*) ppaf24->block, 8 * ppaf24->channels) ;
 
 	/* Unpack block. */
 	for (k = 0 ; k < PAF24_SAMPLES_PER_BLOCK * ppaf24->channels ; k++)
@@ -652,7 +651,7 @@ paf24_write_block (SF_PRIVATE *psf, PAF24_PRIVATE *ppaf24)
 
 		/* Do endian swapping if necessary. */
 		if (psf->endian == SF_ENDIAN_BIG)
-			endswap_int_array (ppaf24->data, 8 * ppaf24->channels) ;
+			endswap_int_array ((int*) ppaf24->block, 8 * ppaf24->channels) ;
 		}
 	else if (CPU_IS_BIG_ENDIAN)
 	{	/* This is correct. */
@@ -664,8 +663,8 @@ paf24_write_block (SF_PRIVATE *psf, PAF24_PRIVATE *ppaf24)
 			cptr [1] = nextsample >> 8 ;
 			cptr [2] = nextsample >> 16 ;
 			} ;
-		if (psf->endian == SF_ENDIAN_BIG)
-			endswap_int_array (ppaf24->data, 8 * ppaf24->channels) ;
+		if (psf->endian == SF_ENDIAN_LITTLE)
+			endswap_int_array ((int*) ppaf24->block, 8 * ppaf24->channels) ;
 		} ;
 
 	/* Write block to disk. */

commit 951718c4cd168e53260b17268c3eb3151de9b16a

src/paf.c: Fix alignment issues compiling for Arm
Fix an issue introduced in commit a1f41d90bf.

Earlier versions of Arm (before armv7) did not handle un-aligned
accesses so that GCC warns about casts that increase alignment
requirements (like casts from `char*` to `int*`). Fix this by changing
the pointer from `char*` to `int*` and casting back to `char*` as needed
instead of doing the reverse.

src/paf.c
@@ -63,7 +63,7 @@ typedef struct
 	int				read_block, write_block, read_count, write_count ;
 	sf_count_t		sample_count ;
 	int				*samples ;
-	unsigned char	*block ;
+	int				*block ;
 	int				data [] ; /* ISO C99 struct flexible array. */
 } PAF24_PRIVATE ;
 
@@ -362,7 +362,7 @@ paf24_init (SF_PRIVATE *psf)
 
 	ppaf24->channels	= psf->sf.channels ;
 	ppaf24->samples		= ppaf24->data ;
-	ppaf24->block		= (unsigned char*) (ppaf24->data + PAF24_SAMPLES_PER_BLOCK * ppaf24->channels) ;
+	ppaf24->block		= ppaf24->data + PAF24_SAMPLES_PER_BLOCK * ppaf24->channels ;
 
 	ppaf24->blocksize = PAF24_BLOCK_SIZE * ppaf24->channels ;
 
@@ -498,12 +498,12 @@ paf24_read_block (SF_PRIVATE *psf, PAF24_PRIVATE *ppaf24)
 
 	/* Do endian swapping if necessary. */
 	if ((CPU_IS_BIG_ENDIAN && psf->endian == SF_ENDIAN_LITTLE) || (CPU_IS_LITTLE_ENDIAN && psf->endian == SF_ENDIAN_BIG))
-		endswap_int_array ((int*) ppaf24->block, 8 * ppaf24->channels) ;
+		endswap_int_array (ppaf24->block, 8 * ppaf24->channels) ;
 
 	/* Unpack block. */
 	for (k = 0 ; k < PAF24_SAMPLES_PER_BLOCK * ppaf24->channels ; k++)
 	{	channel = k % ppaf24->channels ;
-		cptr = ppaf24->block + PAF24_BLOCK_SIZE * channel + 3 * (k / ppaf24->channels) ;
+		cptr = ((unsigned char *) ppaf24->block) + PAF24_BLOCK_SIZE * channel + 3 * (k / ppaf24->channels) ;
 		ppaf24->samples [k] = (cptr [0] << 8) | (cptr [1] << 16) | (((unsigned) cptr [2]) << 24) ;
 		} ;
 
@@ -642,7 +642,7 @@ paf24_write_block (SF_PRIVATE *psf, PAF24_PRIVATE *ppaf24)
 	if (CPU_IS_LITTLE_ENDIAN)
 	{	for (k = 0 ; k < PAF24_SAMPLES_PER_BLOCK * ppaf24->channels ; k++)
 		{	channel = k % ppaf24->channels ;
-			cptr = ppaf24->block + PAF24_BLOCK_SIZE * channel + 3 * (k / ppaf24->channels) ;
+			cptr = ((unsigned char *) ppaf24->block) + PAF24_BLOCK_SIZE * channel + 3 * (k / ppaf24->channels) ;
 			nextsample = ppaf24->samples [k] >> 8 ;
 			cptr [0] = nextsample ;
 			cptr [1] = nextsample >> 8 ;
@@ -651,20 +651,20 @@ paf24_write_block (SF_PRIVATE *psf, PAF24_PRIVATE *ppaf24)
 
 		/* Do endian swapping if necessary. */
 		if (psf->endian == SF_ENDIAN_BIG)
-			endswap_int_array ((int*) ppaf24->block, 8 * ppaf24->channels) ;
+			endswap_int_array (ppaf24->block, 8 * ppaf24->channels) ;
 		}
 	else if (CPU_IS_BIG_ENDIAN)
 	{	/* This is correct. */
 		for (k = 0 ; k < PAF24_SAMPLES_PER_BLOCK * ppaf24->channels ; k++)
 		{	channel = k % ppaf24->channels ;
-			cptr = ppaf24->block + PAF24_BLOCK_SIZE * channel + 3 * (k / ppaf24->channels) ;
+			cptr = ((unsigned char *) ppaf24->block) + PAF24_BLOCK_SIZE * channel + 3 * (k / ppaf24->channels) ;
 			nextsample = ppaf24->samples [k] >> 8 ;
 			cptr [0] = nextsample ;
 			cptr [1] = nextsample >> 8 ;
 			cptr [2] = nextsample >> 16 ;
 			} ;
 		if (psf->endian == SF_ENDIAN_LITTLE)
-			endswap_int_array ((int*) ppaf24->block, 8 * ppaf24->channels) ;
+			endswap_int_array (ppaf24->block, 8 * ppaf24->channels) ;
 		} ;
 
 	/* Write block to disk. */