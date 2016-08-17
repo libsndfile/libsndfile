9b980334fb5975e928028a7bd17a10a693a80fbf
evpobr <evpobr@gmail.com>
2016-08-17 21:07:21 +0500

    Fix EXTERNAL_XIPH_LIBS detection

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -12,28 +12,30 @@ endif(VORBIS_INCLUDE_DIR)
 
 find_package(Ogg)
 if(OGG_FOUND)
-	find_path(VORBIS_INCLUDE_DIR vorbis/vorbisfile.h)
+	find_path(VORBIS_INCLUDE_DIR vorbis/codec.h vorbis/vorbisfile.h vorbis/vorbisfile.h)
 	# MSVC built vorbis may be named vorbis_static
 	# The provided project files name the library with the lib prefix.
 	find_library(VORBIS_LIBRARY
 	    NAMES vorbis vorbis_static libvorbis libvorbis_static)
 	find_library(VORBISFILE_LIBRARY
 	    NAMES vorbisfile vorbisfile_static libvorbisfile libvorbisfile_static)
+	find_library(VORBISENC_LIBRARY
+	    NAMES vorbisenc vorbisenc_static libvorbisenc libvorbisenc_static)
 	# Handle the QUIETLY and REQUIRED arguments and set VORBIS_FOUND
 	# to TRUE if all listed variables are TRUE.
 	include(FindPackageHandleStandardArgs)
 	find_package_handle_standard_args(VORBIS DEFAULT_MSG
 	    VORBIS_INCLUDE_DIR
-	    VORBIS_LIBRARY VORBISFILE_LIBRARY)
+	    VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
 endif(OGG_FOUND)
 
 if(VORBIS_FOUND)
-  set(VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY}
+  set(VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} ${VORBISENC_LIBRARY}
         ${OGG_LIBRARY})
 else(VORBIS_FOUND)
   set(VORBIS_LIBRARIES)
 endif(VORBIS_FOUND)
 
 mark_as_advanced(VORBIS_INCLUDE_DIR)
-mark_as_advanced(VORBIS_LIBRARY VORBISFILE_LIBRARY)
+mark_as_advanced(VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
 
