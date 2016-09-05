173da3877242c8591c92e1c6e6d55def0447f171
evpobr <evpobr@gmail.com>
2016-09-05 21:31:22 +0500

    Improve Speex search with PkgConfig

--- cmake/FindSpeex.cmake
+++ cmake/FindSpeex.cmake
@@ -2,8 +2,11 @@ if (SPEEX_INCLUDE_DIR)
 	set (SPEEX_FIND_QUIETLY TRUE)
 endif ()
 
-find_path (SPEEX_INCLUDE_DIR speex/speex.h)
-find_library (SPEEX_LIBRARY NAMES speex)
+find_package (PkgConfig)
+pkg_check_modules(PC_SPEEX QUIET speex)
+
+find_path (SPEEX_INCLUDE_DIR speex/speex.h HINTS ${PC_SPEEX_INCLUDEDIR} ${PC_SPEEX_INCLUDE_DIRS})
+find_library (SPEEX_LIBRARY NAMES speex HINTS ${PC_SPEEX_LIBDIR} ${PC_SPEEX_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SPEEX DEFAULT_MSG SPEEX_INCLUDE_DIR SPEEX_LIBRARY)
