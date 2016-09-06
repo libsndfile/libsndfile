82684f9aff57b37ef17e784f3f7eb55bf570bc60
evpobr <evpobr@gmail.com>
2016-09-06 11:47:33 +0500

    Refactor Vorbis find module

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -1,17 +1,17 @@
 # - Find vorbis
 # Find the native vorbis includes and libraries
 #
-#  VORBIS_INCLUDE_DIR - where to find vorbis.h, etc.
-#  VORBIS_LIBRARIES   - List of libraries when using vorbis(file).
-#  VORBIS_FOUND       - True if vorbis found.
+#  VORBIS_INCLUDE_DIRS - where to find vorbis.h, etc.
+#  VORBIS_LIBRARIES    - List of libraries when using vorbis(file).
+#  VORBIS_FOUND        - True if vorbis found.
 
 if(VORBIS_INCLUDE_DIR)
-    # Already in cache, be silent
-    set(VORBIS_FIND_QUIETLY TRUE)
+	# Already in cache, be silent
+	set(VORBIS_FIND_QUIETLY TRUE)
 endif(VORBIS_INCLUDE_DIR)
 
-find_package(Ogg)
-if(OGG_FOUND)
+find_package (Ogg)
+if (OGG_FOUND)
 	find_path(VORBIS_INCLUDE_DIR vorbis/codec.h vorbis/vorbisfile.h vorbis/vorbisfile.h)
 	# MSVC built vorbis may be named vorbis_static
 	# The provided project files name the library with the lib prefix.
@@ -29,13 +29,10 @@ if(OGG_FOUND)
 	    VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
 endif(OGG_FOUND)
 
-if(VORBIS_FOUND)
-  set(VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} ${VORBISENC_LIBRARY}
-        ${OGG_LIBRARY})
-else(VORBIS_FOUND)
-  set(VORBIS_LIBRARIES)
+if (VORBIS_FOUND)
+	set (VORBIS_INCLUDE_DIRS VORBIS_INCLUDE_DIR)
+	set (VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} ${VORBISENC_LIBRARY}
+		${OGG_LIBRARY})
 endif(VORBIS_FOUND)
 
-mark_as_advanced(VORBIS_INCLUDE_DIR)
-mark_as_advanced(VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
-
+mark_as_advanced(VORBIS_INCLUDE_DIR VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
