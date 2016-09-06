d46fbfe1fbbb1ce66bdbbff2cf69c69c901c8df6
evpobr <evpobr@gmail.com>
2016-09-06 12:08:23 +0500

    Improve Vorbis search with PkgConfig

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -12,15 +12,24 @@ endif(VORBIS_INCLUDE_DIR)
 
 find_package (Ogg)
 if (OGG_FOUND)
-	find_path(VORBIS_INCLUDE_DIR vorbis/codec.h vorbis/vorbisfile.h vorbis/vorbisfile.h)
+	find_package (PkgConfig)
+	pkg_check_modules(PC_VORBIS QUIET vorbis)
+	pkg_check_modules(PC_VORBISFILE QUIET vorbisfile)
+	pkg_check_modules(PC_VORBISENC QUIET vorbisenc)
+
+	find_path(VORBIS_INCLUDE_DIR vorbis/codec.h vorbis/vorbisfile.h vorbis/vorbisfile.h
+		HINTS ${PC_VORBIS_INCLUDEDIR} ${PC_VORBIS_INCLUDE_DIRS})
 	# MSVC built vorbis may be named vorbis_static
 	# The provided project files name the library with the lib prefix.
 	find_library(VORBIS_LIBRARY
-	    NAMES vorbis vorbis_static libvorbis libvorbis_static)
+	    NAMES vorbis vorbis_static libvorbis libvorbis_static
+		HINTS ${PC_VORBIS_LIBDIR} ${PC_VORBIS_LIBRARY_DIRS})
 	find_library(VORBISFILE_LIBRARY
-	    NAMES vorbisfile vorbisfile_static libvorbisfile libvorbisfile_static)
+	    NAMES vorbisfile vorbisfile_static libvorbisfile libvorbisfile_static
+		HINTS ${PC_VORBISFILE_LIBDIR} ${PC_VORBISFILE_LIBRARY_DIRS})
 	find_library(VORBISENC_LIBRARY
-	    NAMES vorbisenc vorbisenc_static libvorbisenc libvorbisenc_static)
+	    NAMES vorbisenc vorbisenc_static libvorbisenc libvorbisenc_static
+		HINTS ${PC_VORBISENC_LIBDIR} ${PC_VORBISENC_LIBRARY_DIRS})
 	# Handle the QUIETLY and REQUIRED arguments and set VORBIS_FOUND
 	# to TRUE if all listed variables are TRUE.
 	include(FindPackageHandleStandardArgs)
