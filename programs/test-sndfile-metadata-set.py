56c1eb3752c5c07e5da23379cbc4386998b1f76d
David Seifert <soap@gentoo.org>
2017-01-27 23:51:19 +0100

    Make 'test-sndfile-metadata-set.py' py2/py3 compatible
    '#!/usr/bin/env python' constructs are more portable, as they
    allow the user to override which python executable is called.
    See also:
    https://blogs.gentoo.org/mgorny/2016/02/08/a-quick-note-on-portable-shebangs/

--- programs/test-sndfile-metadata-set.py
+++ programs/test-sndfile-metadata-set.py
@@ -32,7 +32,16 @@
 
 # Simple test script for the sndfile-metadata-set program.
 
-import commands, os, sys
+from __future__ import print_function
+
+try:
+    # py2
+    import commands
+except ImportError:
+    # py3
+    import subprocess as commands
+
+import os, sys
 import time, datetime
 
 class Programs:
@@ -48,14 +57,14 @@ class Programs:
     def _run_command (self, should_fail, cmd):
         status, output = commands.getstatusoutput (cmd)
         if should_fail and not status:
-            print "\n\nError : command '%s' should have failed." % cmd
-            print output
-            print ""
+            print("\n\nError : command '%s' should have failed." % cmd)
+            print(output)
+            print()
             sys.exit (1)
         if not should_fail and status:
-            print "\n\nError : command '%s' should not have failed." % cmd
-            print output
-            print ""
+            print("\n\nError : command '%s' should not have failed." % cmd)
+            print(output)
+            print()
             sys.exit (1)
         return output
 
@@ -71,18 +80,18 @@ class Programs:
     def check_executables (self):
         for name in [ self.meta_set_prog, self.meta_get_prog, self.make_sine_prog ]:
             if not (os.path.isfile (name)):
-                print "\n\nError : Can't find executable '%s'. Have you run make?" % name
+                print("\n\nError : Can't find executable '%s'. Have you run make?" % name)
                 sys.exit (1)
 
 
 def print_test_name (name):
-    print "    %-30s :" % name,
+    print("    %-30s :" % name, end="")
 
 def assert_info (programs, filename, arg, value):
     output = programs.meta_get (False, "%s %s" % (arg, filename))
     if output.find (value) < 0:
-        print "\n\nError : not able to find '%s'." % value
-        print output
+        print("\n\nError : not able to find '%s'." % value)
+        print(output)
         sys.exit (1)
     return
 
@@ -90,33 +99,33 @@ def assert_info (programs, filename, arg, value):
 def test_empty_fail (programs):
     print_test_name ("Empty fail test")
     output = programs.meta_set (True, "--bext-description Alpha sine.wav")
-    print "ok"
+    print("ok")
 
 def test_copy (programs):
     print_test_name ("Copy test")
     output = programs.meta_set (False, "--bext-description \"First Try\" sine.wav output.wav")
     assert_info (programs, "output.wav", "--bext-description", "First Try")
-    print "ok"
+    print("ok")
 
 def test_update (programs, tests):
     print_test_name ("Update test")
     for arg, value in tests:
         output = programs.meta_set (False, "%s \"%s\" output.wav" % (arg, value))
         assert_info (programs, "output.wav", arg, value)
-    print "ok"
+    print("ok")
 
 def test_post_mod (programs, tests):
     print_test_name ("Post mod test")
     for arg, value in tests:
         assert_info (programs, "output.wav", arg, value)
-    print "ok"
+    print("ok")
 
 def test_auto_date (programs):
     print_test_name ("Auto date test")
     output = programs.meta_set (False, "--bext-auto-time-date sine.wav date-time.wav")
     target = datetime.date.today ().__str__ ()
     assert_info (programs, "date-time.wav", "--bext-orig-date", target)
-    print "ok"
+    print("ok")
 
 
 #-------------------------------------------------------------------------------
@@ -125,7 +134,7 @@ def test_coding_history (programs):
     print_test_name ("Coding history test")
     output = programs.meta_set (False, "--bext-coding-hist \"alpha beta\" output.wav")
     output = programs.meta_get (False, "--bext-coding-hist output.wav")
-    print "ok"
+    print("ok")
 
 #-------------------------------------------------------------------------------
 
@@ -135,15 +144,15 @@ def test_rewrite (programs):
     output = programs.meta_set (False, "--bext-originator \"Short\" output.wav")
     output = programs.meta_get (False, "--bext-originator output.wav")
     if output.find ("really long") > 0:
-        print "\n\nError : output '%s' should not contain 'really long'." % output
+        print("\n\nError : output '%s' should not contain 'really long'." % output)
         sys.exit (1)
-    print "ok"
+    print("ok")
 
 #===============================================================================
 
 test_dir = "programs"
 
-print "\nTesting WAV metadata manipulation:"
+print("\nTesting WAV metadata manipulation:")
 
 if os.path.isdir (test_dir):
     os.chdir (test_dir)
@@ -159,7 +168,7 @@ programs.check_executables ()
 
 programs.make_sine ()
 if not os.path.isfile ("sine.wav"):
-    print "\n\nError : Can't file file 'sine.wav'."
+    print("\n\nError : Can't file file 'sine.wav'.")
     sys.exit (1)
 
 test_empty_fail (programs)
@@ -183,7 +192,7 @@ test_update (programs, [ ("--str-artist", "Fox") ])
 test_rewrite (programs)
 
 
-print ""
+print()
 
 sys.exit (0)
 
