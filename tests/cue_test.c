e1439a7fc50a4ba5717463c9eeff3fd29c7f7021
Diemo Schwarz <schwarz@ircam.fr>
2017-02-20 18:42:28 +0100

    test case for getting variable number of cues on reference wav file 5 Squashed commits: [a6e7e0b] test more variants [a58d3a9] first test case on reference wav file [5c16267] correct error reporting [b30b214] adapt cue test to canonical cue api from 1.0.27 [306499e] correct error reporting and expected times

---
+++ tests/cue_test.c
@@ -0,0 +1,74 @@
+
+#include "sfconfig.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sndfile.h>
+
+
+static int test_cues (const char *filename);
+int test_cues (const char *filename)
+{
+    SNDFILE    *file;
+    SF_INFO	sfinfo;
+
+    int i, err;
+    uint32_t count = 0;
+    SF_CUES_VAR(0) *info;
+
+    if ((file = sf_open(filename, SFM_READ, &sfinfo)) == NULL)
+    {
+	printf("can't open file '%s'\n", filename);
+	return 0;
+    }
+
+    printf("\n---- get cues of file '%s'\n", filename);
+
+    if ((err = sf_command(file, SFC_GET_CUE_COUNT, &count, sizeof(uint32_t))) == SF_FALSE)
+    {
+	if (sf_error(file))
+	    printf("can't get cue info size for file '%s' (arg size %lu), err %s\n", 
+		   filename, sizeof(uint32_t), sf_strerror(file));
+	else
+	    printf("no cue info for file '%s'\n", filename);
+	return 0;
+    }
+	
+    int size = sizeof(*info) + count * sizeof(SF_CUE_POINT);
+    printf("num. cues in file '%s': %d  info struct size %d\n", filename, count, size);
+
+    if (!(info = malloc(size)))
+	return 0;
+
+    if (sf_command(file, SFC_GET_CUE, info, size) == SF_FALSE)
+    {
+	printf("can't get cue info of size %d for file '%s' error %s\n", 
+	       size, filename, sf_strerror(file));
+	return 0;
+    }
+
+    printf("number of cues %d  in struct\n", info->cue_count);
+
+    for (i = 0; i < info->cue_count; i++)
+    {
+	int    pos = info->cue_points[i].position;
+	double t   = (double) pos / sfinfo.samplerate;
+	double expected = i < 8  ?  (double) i / 3.  :  10. / 3.;
+
+	printf("cue %02d: markerID %02d  position %06d  (time %.3f  expected %.3f  diff %f)  label '%s'\n",
+	       i, info->cue_points[i].indx, pos, t, expected, (double) fabs(t - expected), info->cue_points[i].name);
+    }
+    
+    sf_close(file);
+
+    return 1;
+}
+
+
+int main (int argc, char **argv)
+{
+    test_cues("clickpluck24.wav");
+    test_cues("clickpluck.wav");
+    test_cues("clickpluck.aiff");
+}
