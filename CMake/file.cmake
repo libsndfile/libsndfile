51dee4fb1bddffcd657e5d2460955d1f82af3e87
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-03-11 15:51:21 +0100

    cmake: don't use sed and grep to calculate version and count lines

--- CMake/file.cmake
+++ CMake/file.cmake
@@ -1,27 +1,19 @@
 
+function(lsf_version_majorversion LIBVERSION LIBVERSIONMAJOR)
+	file(READ "${CMAKE_SOURCE_DIR}/configure.ac" CONF_AC_CONTENT)
+	string(REGEX MATCH "AC_INIT\\(\\[libsndfile\\],\\[([0-9a-zA-Z\\.]+)\\]" MATCH_AC_INIT "{${CONF_AC_CONTENT}")
+	set(LIBVERSION_VAL "${CMAKE_MATCH_1}")
+	string(REGEX MATCH "^([0-9]+)" MATCH_MAJOR "${LIBVERSION_VAL}")
+	if(NOT MATCH_AC_INIT OR NOT MATCH_MAJOR)
+		message(FATAL_ERROR "Could not determine version")
+		endif()
+	set("${LIBVERSION}" "${LIBVERSION_VAL}" PARENT_SCOPE)
+	set("${LIBVERSIONMAJOR}" "${MATCH_MAJOR}" PARENT_SCOPE)
+	endfunction()
+
 function (file_line_count filename variable)
-	# Assume `find_progam (WC wc)` has already set this.
-	if (NOT WC)
-		message (FATAL_ERROR "Need the 'wc' program to find line coount.")
-		endif ()
-
-	if (NOT SED)
-		message (FATAL_ERROR "Need the 'sed' program to find line coount.")
-		endif ()
-
-	if (NOT (EXISTS "${filename}"))
-		message (FATAL_ERROR "File ${filename} does not exist.")
-		endif ()
-
-	execute_process (
-		COMMAND ${WC} -l ${filename}
-		COMMAND ${SED} "s/^[ ]*//"    # wc output on Mac has leading whitespace.
-		COMMAND ${SED} "s/ .*//"
-		OUTPUT_VARIABLE line_count
-		)
-
-	# Tedious!
-	string (STRIP ${line_count} line_count)
+	file(STRINGS "${filename}" filename_lines)
+	list(LENGTH filename_lines line_count)
 
 	set (${variable} ${line_count} PARENT_SCOPE)
 	endfunction ()
