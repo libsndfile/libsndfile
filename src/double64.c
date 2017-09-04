574c97c7f723672f1d73a119ec73dbfb2cbeea80
eblanca <eblanca76@users.sourceforge.net>
2017-09-04 19:47:32 +0200

    fix clipping of negative values

--- src/double64.c
+++ src/double64.c
@@ -519,7 +519,7 @@ d2i_clip_array (const double *src, int count, int *dest, double scale)
 
 		if (CPU_CLIPS_POSITIVE == 0 && tmp > (1.0 * INT_MAX))
 			dest [count] = INT_MAX ;
-		else if (CPU_CLIPS_NEGATIVE == 0 && tmp < (-1.0 * INT_MAX))
+		else if (CPU_CLIPS_NEGATIVE == 0 && tmp < (1.0 * INT_MIN))
 			dest [count] = INT_MIN ;
 		else
 			dest [count] = lrint (tmp) ;
--- src/float32.c
+++ src/float32.c
@@ -470,7 +470,7 @@ f2i_clip_array (const float *src, int count, int *dest, float scale)
 
 		if (CPU_CLIPS_POSITIVE == 0 && tmp > (1.0 * INT_MAX))
 			dest [count] = INT_MAX ;
-		else if (CPU_CLIPS_NEGATIVE == 0 && tmp < (-1.0 * INT_MAX))
+		else if (CPU_CLIPS_NEGATIVE == 0 && tmp < (1.0 * INT_MIN))
 			dest [count] = INT_MIN ;
 		else
 			dest [count] = lrintf (tmp) ;
