706e86d1b22c2ea8f4621fe1273321a40736077f
evpobr <evpobr@gmail.com>
2018-08-19 08:56:17 +0500

    CMake: updates & fixes

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -5,25 +5,52 @@
 #  SQLITE3_LIBRARIES    - List of libraries when using SQLITE3.
 #  SQLITE3_FOUND        - True if SQLITE3 found.
 
-if(SQLITE3_INCLUDE_DIR)
+if (SQLITE3_INCLUDE_DIR)
     # Already in cache, be silent
-    set(SQLITE3_FIND_QUIETLY TRUE)
-endif(SQLITE3_INCLUDE_DIR)
+    set (SQLITE3_FIND_QUIETLY TRUE)
+endif ()
 
 find_package (PkgConfig QUIET)
-pkg_check_modules(PC_SQLITE3 QUIET sqlite3)
+pkg_check_modules (PC_SQLITE3 QUIET sqlite3)
 
-find_path(SQLITE3_INCLUDE_DIR sqlite3.h HINTS ${PC_SQLITE3_INCLUDEDIR} ${PC_SQLITE3_INCLUDE_DIRS} ${SQLITE3_ROOT})
+set (SQLITE3_VERSION ${PC_SQLITE3_VERSION})
 
-find_library (SQLITE3_LIBRARY NAMES sqlite3 HINTS ${PC_SQLITE3_LIBDIR} ${PC_SQLITE3_LIBRARY_DIRS} ${SQLITE3_ROOT} PATH_SUFFIXES lib)
+find_path (SQLITE3_INCLUDE_DIR sqlite3.h
+    HINTS
+        ${PC_SQLITE3_INCLUDEDIR}
+        ${PC_SQLITE3_INCLUDE_DIRS}
+        ${SQLITE3_ROOT}
+    )
 
-include(FindPackageHandleStandardArgs)
+find_library (SQLITE3_LIBRARY
+    NAMES
+        sqlite3
+    HINTS
+        ${PC_SQLITE3_LIBDIR}
+        ${PC_SQLITE3_LIBRARY_DIRS}
+        ${SQLITE3_ROOT}
+    )
 
-find_package_handle_standard_args(SQLITE3 DEFAULT_MSG SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY)
+include (FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args (SQLITE3
+    REQUIRED_VARS
+        SQLITE3_LIBRARY
+        SQLITE3_INCLUDE_DIR
+    VERSION_VAR
+        SQLITE3_VERSION
+    )
 
 if (SQLITE3_FOUND)
 	set (SQLITE3_INCLUDE_DIRS ${SQLITE3_INCLUDE_DIR})
-	set (SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
-endif (SQLITE3_FOUND)
+    set (SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
+	if (NOT TARGET SQLite3::SQLite3)
+        add_library (SQLite3::SQLite3 UNKNOWN IMPORTED)
+        set_target_properties (SQLite3::SQLite3 PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${SQLITE3_INCLUDE_DIRS}"
+            IMPORTED_LOCATION "${SQLITE3_LIBRARIES}"
+        )
+    endif ()
+endif ()
 
-mark_as_advanced(SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY)
+mark_as_advanced (SQLITE3_INCLUDE_DIR SQLITE3_LIBRARY)
--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -1,49 +1,64 @@
-# - Find vorbis
-# Find the native vorbis includes and libraries
+# - Find vorbisenc
+# Find the native vorbisenc includes and libraries
 #
 #  VORBIS_INCLUDE_DIRS - where to find vorbis.h, etc.
-#  VORBIS_LIBRARIES    - List of libraries when using vorbis(file).
-#  Vorbis_FOUND        - True if vorbis found.
+#  VORBIS_LIBRARIES    - List of libraries when using vorbis.
+#  VORBIS_FOUND        - True if vorbis found.
 
-if(VORBIS_INCLUDE_DIR)
+if (VORBIS_INCLUDE_DIR)
 	# Already in cache, be silent
-	set(VORBIS_FIND_QUIETLY TRUE)
-endif(VORBIS_INCLUDE_DIR)
+	set (VORBIS_FIND_QUIETLY TRUE)
+endif ()
 
 find_package (Ogg QUIET)
 
 find_package (PkgConfig QUIET)
-pkg_check_modules(PC_VORBIS QUIET vorbis)
-pkg_check_modules(PC_VORBISFILE QUIET vorbisfile)
-pkg_check_modules(PC_VORBISENC QUIET vorbisenc)
+pkg_check_modules (PC_VORBIS QUIET vorbis)
 
-find_path(VORBIS_INCLUDE_DIR vorbis/codec.h vorbis/vorbisfile.h vorbis/vorbisfile.h
-	HINTS ${PC_VORBIS_INCLUDEDIR} ${PC_VORBIS_INCLUDE_DIRS} ${VORBIS_ROOT}
-	PATH_SUFFIXES include)
-# MSVC built vorbis may be named vorbis_static
-# The provided project files name the library with the lib prefix.
-find_library(VORBIS_LIBRARY
-	NAMES vorbis vorbis_static libvorbis libvorbis_static
-	HINTS ${PC_VORBIS_LIBDIR} ${PC_VORBIS_LIBRARY_DIRS} ${VORBIS_ROOT}
-	PATH_SUFFIXES lib)
-find_library(VORBISFILE_LIBRARY
-	NAMES vorbisfile vorbisfile_static libvorbisfile libvorbisfile_static
-	HINTS ${PC_VORBISFILE_LIBDIR} ${PC_VORBISFILE_LIBRARY_DIRS} ${VORBIS_ROOT}
-	PATH_SUFFIXES lib)
-find_library(VORBISENC_LIBRARY
-	NAMES vorbisenc vorbisenc_static libvorbisenc libvorbisenc_static
-	HINTS ${PC_VORBISENC_LIBDIR} ${PC_VORBISENC_LIBRARY_DIRS} ${VORBIS_ROOT}
-	PATH_SUFFIXES lib)
+set (VORBIS_VERSION ${PC_VORBIS_VERSION})
+
+find_path (VORBIS_INCLUDE_DIR vorbis/codec.h
+	HINTS
+		${PC_VORBIS_INCLUDEDIR}
+		${PC_VORBIS_INCLUDE_DIRS}
+		${VORBIS_ROOT}
+	)
+
+find_library (VORBIS_LIBRARY
+	NAMES
+		vorbis
+		vorbis_static
+		libvorbis
+		libvorbis_static
+	HINTS
+		${PC_VORBIS_LIBDIR}
+		${PC_VORBIS_LIBRARY_DIRS}
+		${VORBIS_ROOT}
+	)
 
 # Handle the QUIETLY and REQUIRED arguments and set VORBIS_FOUND
 # to TRUE if all listed variables are TRUE.
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Vorbis DEFAULT_MSG VORBIS_INCLUDE_DIR VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (Vorbis
+	REQUIRED_VARS
+		VORBIS_LIBRARY
+		VORBIS_INCLUDE_DIR
+		OGG_FOUND
+	VERSION_VAR
+        VORBIS_VERSION
+	)
 
 if (VORBIS_FOUND)
-	set (VORBIS_INCLUDE_DIRS ${VORBIS_INCLUDE_DIR} ${OGG_INCLUDE_DIRS})
-	set (VORBIS_LIBRARIES ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} ${VORBISENC_LIBRARY}
-		${OGG_LIBRARIES})
-endif (VORBIS_FOUND)
+	set (VORBIS_INCLUDE_DIRS ${VORBIS_INCLUDE_DIR})
+	set (VORBIS_LIBRARIES ${VORBIS_LIBRARY} ${OGG_LIBRARIES})
+    if (NOT TARGET Vorbis::Vorbis)
+		add_library (Vorbis::Vorbis UNKNOWN IMPORTED)
+		set_target_properties (Vorbis::Vorbis PROPERTIES
+			INTERFACE_INCLUDE_DIRECTORIES "${VORBIS_INCLUDE_DIR}"
+			IMPORTED_LOCATION "${VORBIS_LIBRARY}"
+			INTERFACE_LINK_LIBRARIES Ogg::Ogg
+		)
+	endif ()
+endif ()
 
-mark_as_advanced(VORBIS_INCLUDE_DIR VORBIS_LIBRARY VORBISFILE_LIBRARY VORBISENC_LIBRARY)
+mark_as_advanced (VORBIS_INCLUDE_DIR VORBIS_LIBRARY)
