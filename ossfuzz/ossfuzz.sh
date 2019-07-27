e8a6ca9267a802834f1baac2bc66fcacddb38582
Max Dymond <cmeister2@gmail.com>
2019-07-27 20:44:18 +0100

    Add ossfuzz support for libsndfile
    Fixes #476
    
    Add a fuzzer (`sndfile_fuzzer`) which can be compiled and run
    by the OSS-Fuzz infrastructure.

---
+++ ossfuzz/ossfuzz.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+# This script is called by the oss-fuzz main project when compiling the fuzz
+# targets. This script is regression tested by ci_oss.sh.
+
+# Save off the current folder as the build root.
+export BUILD_ROOT=$PWD
+
+echo "CC: ${CC:-}"
+echo "CXX: ${CXX:-}"
+echo "LIB_FUZZING_ENGINE: ${LIB_FUZZING_ENGINE:-}"
+echo "CFLAGS: ${CFLAGS:-}"
+echo "CXXFLAGS: ${CXXFLAGS:-}"
+echo "OUT: ${OUT:-}"
+
+export MAKEFLAGS+="-j$(nproc)"
+
+# Install dependencies
+apt-get -y install autoconf autogen automake libasound2-dev \
+    libflac-dev libogg-dev libopus-dev libtool libvorbis-dev pkg-config python
+
+# Compile the fuzzer.
+./autogen.sh
+./configure --enable-werror --enable-ossfuzzers
+make V=1
+
+# Copy the fuzzer to the output directory.
+cp -v ossfuzz/sndfile_fuzzer $OUT/
