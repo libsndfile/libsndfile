75c68a1cddad71267bc63cf6b268a534d3ca7b99
Nicolas Imbard <nicolas.imbard@engie.com>
2020-02-26 11:01:44 +0100

    opus: Allow setting of OGG page latency
    Latency is set via 'sf_command' using 'SFC_SET_OGG_PAGE_LATENCY_MS'.
    Max average latency set at 1600ms, min average latency set at 50ms.
    Default latency set at 1000ms.
    
    Closes: https://github.com/erikd/libsndfile/pull/512

--- src/sndfile.c
+++ src/sndfile.c
@@ -897,6 +897,7 @@ int
 sf_command	(SNDFILE *sndfile, int command, void *data, int datasize)
 {	SF_PRIVATE *psf = (SF_PRIVATE *) sndfile ;
 	double quality ;
+    double latency ;
 	int old_value ;
 
 	/* This set of commands do not need the sndfile parameter. */
@@ -1376,6 +1377,12 @@ sf_command	(SNDFILE *sndfile, int command, void *data, int datasize)
 			quality = 1.0 - SF_MAX (0.0, SF_MIN (1.0, quality)) ;
 			return sf_command (sndfile, SFC_SET_COMPRESSION_LEVEL, &quality, sizeof (quality)) ;
 
+        case SFC_SET_OGG_PAGE_LATENCY_MS :
+            if (data == NULL || datasize != sizeof (double))
+                return SF_FALSE ;
+
+            latency = *((double *) data) ;
+            return sf_command (sndfile, SFC_SET_OGG_PAGE_LATENCY, &latency, sizeof (latency)) ;
 
 		default :
 			/* Must be a file specific command. Pass it on. */
