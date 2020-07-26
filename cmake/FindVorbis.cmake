73e09a170539116ead8a5a16c6588bb44697a492
evpobr <evpobr@gmail.com>
2020-07-26 13:35:28 +0500

    CMake: Fix Vorbis dependency handling
    * Fix Vorbis not found for CMake < 3.17 (closes #530)
    * Update FindVorbis module documentation
    * Remove unused VorbisEnc.cmake
    * Fix bug in SndFileConfig.cmake.in

--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -1,9 +1,71 @@
-# - Find vorbisenc
-# Find the native vorbisenc includes and libraries
-#
-#  VORBIS_INCLUDE_DIRS - where to find vorbis.h, etc.
-#  VORBIS_LIBRARIES    - List of libraries when using vorbis.
-#  VORBIS_FOUND        - True if vorbis found.
+#[=======================================================================[.rst:
+FindVorbis
+----------
+
+Finds the native vorbis, vorbisenc amd vorbisfile includes and libraries.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``Vorbis::vorbis``
+  The Vorbis library
+``Vorbis::vorbisenc``
+  The VorbisEnc library
+``Vorbis::vorbisfile``
+  The VorbisFile library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``Vorbis_Vorbis_INCLUDE_DIRS``
+  List of include directories when using vorbis.
+``Vorbis_Enc_INCLUDE_DIRS``
+  List of include directories when using vorbisenc.
+``Vorbis_File_INCLUDE_DIRS``
+  List of include directories when using vorbisfile.
+``Vorbis_Vorbis_LIBRARIES``
+  List of libraries when using vorbis.
+``Vorbis_Enc_LIBRARIES``
+  List of libraries when using vorbisenc.
+``Vorbis_File_LIBRARIES``
+  List of libraries when using vorbisfile.
+``Vorbis_FOUND``
+  True if vorbis and requested components found.
+``Vorbis_Vorbis_FOUND``
+  True if vorbis found.
+``Vorbis_Enc_FOUND``
+  True if vorbisenc found.
+``Vorbis_Enc_FOUND``
+  True if vorbisfile found.
+
+Cache variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Vorbis_Vorbis_INCLUDE_DIR``
+  The directory containing ``vorbis/vorbis.h``.
+``Vorbis_Enc_INCLUDE_DIR``
+  The directory containing ``vorbis/vorbisenc.h``.
+``Vorbis_File_INCLUDE_DIR``
+  The directory containing ``vorbis/vorbisenc.h``.
+``Vorbis_Vorbis_LIBRARY``
+  The path to the vorbis library.
+``Vorbis_Enc_LIBRARY``
+  The path to the vorbisenc library.
+``Vorbis_File_LIBRARY``
+  The path to the vorbisfile library.
+
+Hints
+^^^^^
+
+A user may set ``Vorbis_ROOT`` to a vorbis installation root to tell this module where to look.
+
+#]=======================================================================]
 
 if (Vorbis_Vorbis_INCLUDE_DIR)
 	# Already in cache, be silent
@@ -21,7 +83,7 @@ pkg_check_modules (PC_Vorbis_Vorbis QUIET vorbis)
 pkg_check_modules (PC_Vorbis_Enc QUIET vorbisenc)
 pkg_check_modules (PC_Vorbis_File QUIET vorbisfile)
 
-set (Vorbis_VERSION ${PC_VORBIS_VERSION})
+set (Vorbis_VERSION ${PC_Vorbis_Vorbis_VERSION})
 
 find_path (Vorbis_Vorbis_INCLUDE_DIR vorbis/codec.h
 	HINTS
@@ -82,29 +144,26 @@ find_library (Vorbis_File_LIBRARY
 
 include (FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args (Vorbis_Vorbis
+if (Vorbis_Vorbis_LIBRARY AND Vorbis_Vorbis_INCLUDE_DIR AND Ogg_FOUND)
+    set (Vorbis_Vorbis_FOUND TRUE)
+endif ()
+
+if (Vorbis_Enc_LIBRARY AND Vorbis_Enc_INCLUDE_DIR AND Vorbis_Vorbis_FOUND)
+    set (Vorbis_Enc_FOUND TRUE)
+endif ()
+
+if (Vorbis_Vorbis_FOUND AND Vorbis_File_LIBRARY AND Vorbis_File_INCLUDE_DIR)
+    set (Vorbis_File_FOUND TRUE)
+endif ()
+
+find_package_handle_standard_args (Vorbis
 	REQUIRED_VARS
 		Vorbis_Vorbis_LIBRARY
 		Vorbis_Vorbis_INCLUDE_DIR
 		Ogg_FOUND
-	NAME_MISMATCHED
-	)
+	HANDLE_COMPONENTS
+	VERSION_VAR Vorbis_VERSION)
 
-find_package_handle_standard_args (Vorbis_Enc
-	REQUIRED_VARS
-		Vorbis_Enc_LIBRARY
-		Vorbis_Enc_INCLUDE_DIR
-		Vorbis_Vorbis_FOUND
-	NAME_MISMATCHED
-	)
-
-find_package_handle_standard_args (Vorbis_File
-	REQUIRED_VARS
-		Vorbis_File_LIBRARY
-		Vorbis_File_INCLUDE_DIR
-		Vorbis_Vorbis_FOUND
-	NAME_MISMATCHED
-	)
 
 if (Vorbis_Vorbis_FOUND)
 	set (Vorbis_Vorbis_INCLUDE_DIRS ${VORBIS_INCLUDE_DIR})
@@ -145,14 +204,6 @@ if (Vorbis_Vorbis_FOUND)
 	endif ()
 
 endif ()
-
-find_package_handle_standard_args (Vorbis
-	REQUIRED_VARS
-		Vorbis_Vorbis_LIBRARY
-		Vorbis_Vorbis_INCLUDE_DIR
-		Ogg_FOUND
-	HANDLE_COMPONENTS
-	VERSION_VAR Vorbis_VERSION)
 
 mark_as_advanced (Vorbis_Vorbis_INCLUDE_DIR Vorbis_Vorbis_LIBRARY)
 mark_as_advanced (Vorbis_Enc_INCLUDE_DIR Vorbis_Enc_LIBRARY)
