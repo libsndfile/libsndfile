bea8fa5d0eb7470f9df1b7a71d5bb0f0bb079188
evpobr <evpobr@gmail.com>
2021-03-04 20:16:24 +0500

    common: Fix NULL pointer dereferencing

--- src/common.c
+++ src/common.c
@@ -1261,8 +1261,9 @@ typedef SF_CUES_VAR () SF_CUES_0 ;
 SF_CUES *
 psf_cues_alloc (uint32_t cue_count)
 {	SF_CUES *pcues = calloc (1, SF_CUES_VAR_SIZE (cue_count)) ;
-
-	pcues->cue_count = cue_count ;
+	if (pcues)
+	{	pcues->cue_count = cue_count ;
+		} ;
 	return pcues ;
 } /* psf_cues_alloc */
 
