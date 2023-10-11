4ab48d19f6d6ffb2876a0ab7fd319aca914dc404
Alex Stewart <alex.stewart@ni.com>
2023-10-11 11:10:59 -0400

    ossfuzz: fix sometimes undefined default case
    When vfseek() is called with a whence that is not SEEK_SET, SEEK_CUR, or
    SEEK_END, it enters a default switch-case that does not set the
    new_offset variable. The function then continues on and uses that unset
    variable.
    
    Linux 3.1+ stdio allows for SEEK_DATA and SEEK_HOLE whence values,
    though they aren't conceptually supported by the vfseek() function.
    
    Handle this case more sanely (and consistently with the lseek()
    function) by setting errno to EINVAL when whence is an unsupported
    value, and immediately returning -1 to flag the error.
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- ossfuzz/sndfile_fuzz_header.h
+++ ossfuzz/sndfile_fuzz_header.h
@@ -1,6 +1,8 @@
 #ifndef SNDFILE_FUZZ_HEADER_H
 #define SNDFILE_FUZZ_HEADER_H
 
+#include <errno.h>
+
 typedef struct
 {
   sf_count_t offset ;
@@ -32,6 +34,9 @@ static sf_count_t vfseek (sf_count_t offset, int whence, void *user_data)
         break ;
 
     default :
+        // SEEK_DATA and SEEK_HOLE are not supported by this function.
+        errno = EINVAL ;
+        return -1 ;
         break ;
   }
 
