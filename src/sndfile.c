f314f8cbfafc940adc4321ebb252d9246bcf1c42
haruki3hhh <135201297+haruki3hhh@users.noreply.github.com>
2024-07-18 12:59:22 -0500

    Update sndfile.c

--- src/sndfile.c
+++ src/sndfile.c
@@ -1421,6 +1421,7 @@ sf_command	(SNDFILE *sndfile, int command, void *data, int datasize)
 
 		case SFC_GET_CHANNEL_MAP_INFO :
 			if (psf->channel_map == NULL)
+				return SF_FALSE;
 
 			if (data == NULL || datasize != SIGNED_SIZEOF (psf->channel_map [0]) * psf->sf.channels)
 			{	psf->error = SFE_BAD_COMMAND_PARAM ;
